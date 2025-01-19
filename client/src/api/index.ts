99ee3a13f15dd91ee30f97678cc4df41d4c12c9e
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-19 13:32:14 +0100

    Create users api file

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,2 +1,3 @@
 export * from "./customers";
+export * from "./users";
 export * from "./projects";
---
+++ client/src/api/users.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_URL as string;
+
+export const fetchUserData = async () => {
+  const response = await axios.get(`${BASE_URL}/users`);
+  return response.data;
+};
+
+export const createUser = async (user: { name: string; email: string }) => {
+  const response = await axios.post(`${BASE_URL}/users`, user, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response.data;
+};
