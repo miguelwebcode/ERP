7287163cfb0cff5c1ade1fe1f9d450b57370538d
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-20 23:36:41 +0100

    Setup firebase

---
+++ client/src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDZ_LRemHgQdbG3pJA7IOGUbABqOJaFgA",
+  authDomain: "erp-fire.firebaseapp.com",
+  projectId: "erp-fire",
+  storageBucket: "erp-fire.firebasestorage.app",
+  messagingSenderId: "440327598939",
+  appId: "1:440327598939:web:faba0f28a17db1f6fc607e",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
