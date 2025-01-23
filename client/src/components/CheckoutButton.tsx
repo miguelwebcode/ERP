8d60a74f9acfc6b35f586c9f33938c77f4cc8618
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-23 22:44:45 +0100

    Setup firebase

--- client/src/components/CheckoutButton.tsx
+++ client/src/components/CheckoutButton.tsx
@@ -1,4 +1,19 @@
 import { fetchCustomerData } from "../api";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebaseConfig";
+
+/* 
+ TODO: AVERIGUAR QUE ME FALTA PARA PODER LEER FIRESTORE
+*/
+const obtenerClientes = async () => {
+  const querySnapshot = await getDocs(collection(db, "customers"));
+  console.log(querySnapshot);
+  console.log("¿Está vacío?", querySnapshot.empty); // Verifica si hay documentos
+
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+  });
+};
 
 const handleClick = async () => {
   const data = await fetchCustomerData();
@@ -8,7 +23,10 @@ const handleClick = async () => {
 export const CheckoutButton = () => {
   return (
     <button
-      onClick={handleClick}
+      onClick={() => {
+        handleClick();
+        obtenerClientes();
+      }}
       className="bg-slate-600 hover:bg-slate-800 text-white font-bold uppercase p-3 rounded-lg "
     >
       Checkout
