c7564bd4ff73c3b4c5300cb83956b5e80cd75a05
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-24 18:36:27 +0100

    Use logout button component

--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -1,13 +1,9 @@
 import { User } from "firebase/auth";
 import CustomersTestComponent from "./CustomersTestComponent";
-import { logout } from "../../services/auth";
+import { LogoutButton } from "../ui/LogoutButton";
 
 type HomePageProps = {
   user: User;
-};
-
-const handleClick = () => {
-  logout();
 };
 
 export const HomePage = ({ user }: HomePageProps) => {
@@ -15,12 +11,7 @@ export const HomePage = ({ user }: HomePageProps) => {
     <div className="flex flex-col h-[100vh] items-center justify-center p-5">
       <h1>Welcome, {user.email}</h1>
       <CustomersTestComponent />
-      <button
-        className="bg-blue-500 text-white py-2 px-4 mt-5 rounded hover:bg-blue-600"
-        onClick={handleClick}
-      >
-        Logout
-      </button>
+      <LogoutButton />
     </div>
   );
 };
