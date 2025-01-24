c5df5ff4b8038a9313b5ee36f676ada426d970d7
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-24 22:15:26 +0100

    Implement navigation

--- client/src/components/ui/LogoutButton.tsx
+++ client/src/components/ui/LogoutButton.tsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { logout } from "../../services/auth";
 
-const handleClick = () => {
-  logout();
-};
-
 export const LogoutButton = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <button
       className="bg-blue-500 text-white py-2 px-4 mt-5 rounded hover:bg-blue-600"
