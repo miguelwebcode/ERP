5ac9e29542e94a8186dbbe08630efcf103e6d00b
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-24 18:29:19 +0100

    Add console log

--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -27,6 +27,7 @@ export const login = async (email: string, password: string) => {
 export const watchAuthState = (callback: (user: User | null) => void) => {
   onAuthStateChanged(auth, (user) => {
     // El parámetro 'user' ya tiene el tipo 'User | null' gracias al SDK de Firebase
+    console.log("authStateChanged, user is ", user);
     callback(user);
   });
 };
