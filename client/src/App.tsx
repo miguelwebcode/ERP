76aa95ff1aae89f934fa29170a28caf105dfd78f
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-24 12:21:53 +0100

    Modify App layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,29 @@
-import { CheckoutButton } from "./components/CheckoutButton";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import Login from "./components/Login";
+import CustomersTestComponent from "./components/CustomersTestComponent";
+import { watchAuthState } from "./services/auth";
+import Register from "./components/Register";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    watchAuthState(setUser);
+  }, []);
+
   return (
-    <>
-      <div className="flex flex-col h-screen items-center gap-5 p-10 ">
-        <h1>Vite + React + TS + Tailwind - Starter</h1>
-        <CheckoutButton />
-      </div>
-    </>
+    <div>
+      {user ? (
+        <div className="flex flex-col h-[100vh] items-center justify-center p-5">
+          <h1>Welcome, {user.email}</h1>
+          <CustomersTestComponent />
+        </div>
+      ) : (
+        <Register />
+      )}
+    </div>
   );
-}
+};
 
 export default App;
