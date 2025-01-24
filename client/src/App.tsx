797924d9bc6c43412cc1cb170da78977a67b9a07
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-24 18:29:31 +0100

    Use HomePage component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { User } from "firebase/auth";
 import Login from "./components/Login";
-import CustomersTestComponent from "./components/CustomersTestComponent";
 import { watchAuthState } from "./services/auth";
 import Register from "./components/Register";
+import { HomePage } from "./components/HomePage";
 
 const App = () => {
   /* 
@@ -19,10 +19,7 @@ const App = () => {
   return (
     <div>
       {user ? (
-        <div className="flex flex-col h-[100vh] items-center justify-center p-5">
-          <h1>Welcome, {user.email}</h1>
-          <CustomersTestComponent />
-        </div>
+        <HomePage user={user} />
       ) : (
         <>
           {showRegister ? (
