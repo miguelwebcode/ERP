097c7f2e98406622b27dd027e0cfcfad4326b6e2
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-25 19:04:50 +0100

    Create project states data

--- client/src/data/index.ts
+++ client/src/data/index.ts
@@ -1,7 +1,15 @@
-import { Role } from "../types";
+import { ProjectState, Role } from "../types";
 
 export const roles: Role[] = [
   { id: "developer", value: "Developer" },
   { id: "salesman", value: "Salesman" },
   { id: "manager", value: "Manager" },
+];
+
+export const projectStates: ProjectState[] = [
+  { id: "pending", value: "Pending" },
+  { id: "inProgress", value: "In Progress" },
+  { id: "completed", value: "Completed" },
+  { id: "onHold", value: "On Hold" },
+  { id: "cancelled", value: "Cancelled" },
 ];
