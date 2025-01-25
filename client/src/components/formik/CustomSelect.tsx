6c3e29f7a5205cc9ff845dc2a673ff06c43d5315
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-25 17:41:08 +0100

    Style CustomSelect

--- client/src/components/formik/CustomSelect.tsx
+++ client/src/components/formik/CustomSelect.tsx
@@ -4,7 +4,6 @@ import { ReactNode } from "react";
 type CustomSelectProps = {
   label: string;
   name: string;
-  placeholder: string;
   children: ReactNode;
 };
 
@@ -18,16 +17,22 @@ export const CustomSelect = ({
   console.log("meta", meta);
 
   return (
-    <>
-      <label htmlFor={props.name}>{label}</label>
+    <div className="flex flex-col mb-4">
+      <label htmlFor={props.name} className="block text-sm font-medium">
+        {label}
+      </label>
       <select
         {...field}
         {...props}
-        className={meta.error && meta.touched ? "input-error" : ""}
+        className={`w-full p-2 border rounded ${
+          meta.error && meta.touched && "border-red-500"
+        }`}
       >
         {children}
       </select>
-      {meta.error && meta.touched && <div className="error">{meta.error}</div>}
-    </>
+      {meta.error && meta.touched && (
+        <div className="text-red-500 text-sm text-left mt-1">{meta.error}</div>
+      )}
+    </div>
   );
 };
