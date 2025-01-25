d50b114627e856b6220bb74061a373135baefbd8
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-25 19:04:38 +0100

    Create project state type

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,3 +2,8 @@ export type Role = {
   id: string;
   value: "Developer" | "Salesman" | "Manager";
 };
+
+export type ProjectState = {
+  id: string;
+  value: "Pending" | "In Progress" | "Completed" | "On Hold" | "Cancelled";
+};
