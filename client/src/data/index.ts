807355a6ca15171408bf27c6dfb5a401da707f01
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-25 17:40:49 +0100

    Create roles data

---
+++ client/src/data/index.ts
@@ -0,0 +1,7 @@
+import { Role } from "../types";
+
+export const roles: Role[] = [
+  { id: "developer", value: "Developer" },
+  { id: "salesman", value: "Salesman" },
+  { id: "manager", value: "Manager" },
+];
