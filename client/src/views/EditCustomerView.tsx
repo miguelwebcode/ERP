679dd8ace8e97e2253a785e60ce67f52fa1040f0
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 17:08:20 +0100

    Layout Edit Customer view

--- client/src/views/EditCustomerView.tsx
+++ client/src/views/EditCustomerView.tsx
@@ -1,5 +1,22 @@
-import React from "react";
+import CustomerForm from "../components/customers/CustomerForm";
+import EditCustomerForm from "../components/customers/EditCustomerForm";
+import { handleCreateCustomer } from "../services/customers";
+import { useAppStore } from "../stores/app-store";
 
 export const EditCustomerView = () => {
-  return <div>EditCustomerView</div>;
+  const isEditingCustomer = useAppStore((state) => state.isEditingCustomer);
+  return (
+    <div className="flex flex-col md:flex-row justify-center px-5">
+      <EditCustomerForm />
+      {isEditingCustomer && (
+        <CustomerForm
+          titleText="EDIT CUSTOMER"
+          submitButtonText="UPDATE CUSTOMER"
+          onSubmit={handleCreateCustomer}
+        />
+      )}
+    </div>
+  );
 };
+
+export default EditCustomerView;
