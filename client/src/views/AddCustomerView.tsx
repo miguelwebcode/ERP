54b2ee5a711d1ed6c723a146e28da767bb289983
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 17:06:41 +0100

    Pass props to CustomerForm

--- client/src/views/AddCustomerView.tsx
+++ client/src/views/AddCustomerView.tsx
@@ -1,9 +1,14 @@
 import CustomerForm from "../components/customers/CustomerForm";
+import { handleCreateCustomer } from "../services/customers";
 
 export const AddCustomerView = () => {
   return (
     <div className="flex justify-center">
-      <CustomerForm />
+      <CustomerForm
+        titleText="NEW CUSTOMER"
+        submitButtonText="CREATE CUSTOMER"
+        onSubmit={handleCreateCustomer}
+      />
     </div>
   );
 };
