85a5e9ec76a8492eb77bb03f338513fa978b81c0
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 11:27:50 +0100

    Create project views

---
+++ client/src/views/AddProjectView.tsx
@@ -0,0 +1,9 @@
+import ProjectForm from "../components/projects/ProjectForm";
+
+export const AddProjectView = () => {
+  return (
+    <div className="flex justify-center">
+      <ProjectForm />
+    </div>
+  );
+};
---
+++ client/src/views/EditProjectView.tsx
@@ -0,0 +1,3 @@
+export const EditProjectView = () => {
+  return <div>EditProjectView</div>;
+};
