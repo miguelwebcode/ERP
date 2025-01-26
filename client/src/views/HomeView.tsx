15605dbf09b504aae9e930fdd54f7b20d862f339
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 10:57:40 +0100

    Create home buttons

--- client/src/views/HomeView.tsx
+++ client/src/views/HomeView.tsx
@@ -3,6 +3,7 @@ import { LogoutButton } from "../components/ui/LogoutButton";
 import { useEffect } from "react";
 import { useAppStore } from "../stores/app-store";
 import { HomeButton } from "../components/home/HomeButton";
+import { getAllCustomers } from "../services/customers";
 
 export const HomeView = () => {
   const navigate = useNavigate();
@@ -19,15 +20,27 @@ export const HomeView = () => {
       <h1 className="uppercase font-bold">Welcome, {user && user.email} </h1>
       <div className="flex flex-col gap-3 mt-10">
         <HomeButton
-          text="Create Customer"
+          text="Add Customer"
           handleClick={() => {
-            navigate("/customers");
+            navigate("/customers/add");
           }}
         />
         <HomeButton
-          text="Create Project"
+          text="Edit Customer"
           handleClick={() => {
-            navigate("/projects");
+            navigate("/customers/edit");
+          }}
+        />
+        <HomeButton
+          text="Add Project"
+          handleClick={() => {
+            navigate("/projects/add");
+          }}
+        />
+        <HomeButton
+          text="Edit Project"
+          handleClick={() => {
+            navigate("/projects/edit");
           }}
         />
       </div>
