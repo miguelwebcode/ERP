bb134dc897872612e269dd9181b6e3798356618b
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 17:42:05 +0100

    Use edit on submit

--- client/src/views/EditCustomerView.tsx
+++ client/src/views/EditCustomerView.tsx
@@ -1,10 +1,14 @@
+import { FormikHelpers } from "formik";
 import CustomerForm from "../components/customers/CustomerForm";
 import EditCustomerForm from "../components/customers/EditCustomerForm";
-import { handleCreateCustomer } from "../services/customers";
+import { handleEditCustomer } from "../services/customers";
 import { useAppStore } from "../stores/app-store";
+import { CustomerFormValues } from "../types/form-values-types";
 
 export const EditCustomerView = () => {
   const isEditingCustomer = useAppStore((state) => state.isEditingCustomer);
+  const selectedCustomerId = useAppStore((state) => state.selectedCustomerId);
+
   return (
     <div className="flex flex-col md:flex-row justify-center px-5">
       <EditCustomerForm />
@@ -12,7 +16,12 @@ export const EditCustomerView = () => {
         <CustomerForm
           titleText="EDIT CUSTOMER"
           submitButtonText="UPDATE CUSTOMER"
-          onSubmit={handleCreateCustomer}
+          onSubmit={(
+            values: CustomerFormValues,
+            formikHelpers: FormikHelpers<CustomerFormValues>
+          ) => {
+            handleEditCustomer(selectedCustomerId, values, formikHelpers);
+          }}
         />
       )}
     </div>
