99ca260621d22433a9d68f15ff0938ddd88edae3
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 11:28:18 +0100

    Modify Customers view

--- client/src/views/CustomersView.tsx
+++ client/src/views/CustomersView.tsx
@@ -1,9 +1,22 @@
-import CustomerForm from "../components/customers/CustomerForm";
+import { useNavigate } from "react-router-dom";
+import { SharedButton } from "../components/ui/SharedButton";
 
 export const CustomersView = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-center">
-      <CustomerForm />
+      <div className="flex flex-col items-center justify-center bg-white p-6 rounded shadow-md gap-4">
+        <h1 className="uppercase font-bold text-xl">Customer's VIEW</h1>
+
+        <SharedButton
+          text="Add Customer"
+          handleClick={() => navigate("/customers/add")}
+        />
+        <SharedButton
+          text="Edit Customer"
+          handleClick={() => navigate("/customers/edit")}
+        />
+      </div>
     </div>
   );
 };
