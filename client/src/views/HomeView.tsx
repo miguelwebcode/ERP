d9837a43144937d6f0526fb94e67772cea5910cd
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-26 11:28:57 +0100

    Modify Home view

--- client/src/views/HomeView.tsx
+++ client/src/views/HomeView.tsx
@@ -15,35 +15,38 @@ export const HomeView = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center p-5">
-      <h1 className="uppercase font-bold">Welcome, {user && user.email} </h1>
-      <div className="flex flex-col gap-3 mt-10">
-        <SharedButton
-          text="Add Customer"
-          handleClick={() => {
-            navigate("/customers/add");
-          }}
-        />
-        <SharedButton
-          text="Edit Customer"
-          handleClick={() => {
-            navigate("/customers/edit");
-          }}
-        />
-        <SharedButton
-          text="Add Project"
-          handleClick={() => {
-            navigate("/projects/add");
-          }}
-        />
-        <SharedButton
-          text="Edit Project"
-          handleClick={() => {
-            navigate("/projects/edit");
-          }}
-        />
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center justify-center bg-white p-6 rounded shadow-md w-fit">
+        <h1 className="uppercase font-bold text-xl">Customer's VIEW</h1>
+        <h2 className="uppercase font-bold">Welcome, {user && user.email} </h2>
+        <div className="flex flex-col gap-3 mt-4">
+          <SharedButton
+            text="Add Customer"
+            handleClick={() => {
+              navigate("/customers/add");
+            }}
+          />
+          <SharedButton
+            text="Edit Customer"
+            handleClick={() => {
+              navigate("/customers/edit");
+            }}
+          />
+          <SharedButton
+            text="Add Project"
+            handleClick={() => {
+              navigate("/projects/add");
+            }}
+          />
+          <SharedButton
+            text="Edit Project"
+            handleClick={() => {
+              navigate("/projects/edit");
+            }}
+          />
+        </div>
+        <LogoutButton />
       </div>
-      <LogoutButton />
     </div>
   );
 };
