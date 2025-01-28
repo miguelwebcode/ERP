4b065a7f72e12f12024002336c2a55bce89d56e0
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 19:27:55 +0100

    Render CustomerForm when render done

--- client/src/views/AddCustomerView.tsx
+++ client/src/views/AddCustomerView.tsx
@@ -1,19 +1,34 @@
+import { useEffect, useState } from "react";
 import CustomerForm from "../components/customers/CustomerForm";
 import { handleCreateCustomer } from "../services/customers";
+import { useAppStore } from "../stores/app-store";
 
 export const AddCustomerView = () => {
+  const [isRenderDone, setIsRenderDone] = useState(false);
+
+  const setSelectedCustomerId = useAppStore(
+    (state) => state.setSelectedCustomerId
+  );
+
+  useEffect(() => {
+    setSelectedCustomerId("");
+    setIsRenderDone(true);
+  });
   return (
     /* 
      TODO: On render, 
      isEditingCustomer = false
      selectedCustomerId = ""
     */
+
     <div className="flex justify-center">
-      <CustomerForm
-        titleText="NEW CUSTOMER"
-        submitButtonText="CREATE CUSTOMER"
-        onSubmit={handleCreateCustomer}
-      />
+      {isRenderDone && (
+        <CustomerForm
+          titleText="NEW CUSTOMER"
+          submitButtonText="CREATE CUSTOMER"
+          onSubmit={handleCreateCustomer}
+        />
+      )}
     </div>
   );
 };
