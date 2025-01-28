4f8e23c30a78cb11bf2d11304c475dd7eb4d75ca
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 21:17:56 +0100

    Add Customer type

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -7,3 +7,15 @@ export type ProjectState = {
   id: string;
   value: "Pending" | "In Progress" | "Completed" | "On Hold" | "Cancelled";
 };
+
+export type Customer = {
+  address: string;
+  company: string;
+  createdAt: string;
+  customerId: string;
+  email: string;
+  name: string;
+  phone: string;
+  project: string;
+  updatedAt?: string;
+};
