7552f81a44d722e4ae2de50ca96d5f60c0ac3b20
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 19:28:13 +0100

    Render components when render parent done

--- client/src/views/EditCustomerView.tsx
+++ client/src/views/EditCustomerView.tsx
@@ -4,37 +4,54 @@ import EditCustomerForm from "../components/customers/EditCustomerForm";
 import { handleEditCustomer } from "../services/customers";
 import { useAppStore } from "../stores/app-store";
 import { CustomerFormValues } from "../types/form-values-types";
+import { useState, useEffect } from "react";
 
 export const EditCustomerView = () => {
-  const isEditingCustomer = useAppStore((state) => state.isEditingCustomer);
+  /* 
+  TODO: Show form always, form fields disabled until customer fetch is done
+  When this view is render, inside useEffect:
+  isEditingCustomer = false
+  selectedCustomerID = ""
+  
+  When UPDATE is done, inside onSubmit:
+  isEditingCustomer = false
+  selectedCustomerID = ""
+  
+  
+  */
+
+  const [isRenderDone, setIsRenderDone] = useState(false);
   const selectedCustomerId = useAppStore((state) => state.selectedCustomerId);
 
-  /* 
-   TODO: Show form always, form fields disabled until customer fetch is done
-  When this view is render, inside useEffect:
-   isEditingCustomer = false
-   selectedCustomerID = ""
+  const setSelectedCustomerId = useAppStore(
+    (state) => state.setSelectedCustomerId
+  );
 
-   When UPDATE is done, inside onSubmit:
-   isEditingCustomer = false
-   selectedCustomerID = ""
-
-   
-  */
+  useEffect(() => {
+    setSelectedCustomerId("");
+    setIsRenderDone(true);
+  }, []);
   return (
     <div className="flex flex-col md:flex-row justify-center px-5">
-      <EditCustomerForm />
-      {isEditingCustomer && (
-        <CustomerForm
-          titleText="EDIT CUSTOMER"
-          submitButtonText="UPDATE CUSTOMER"
-          onSubmit={(
-            values: CustomerFormValues,
-            formikHelpers: FormikHelpers<CustomerFormValues>
-          ) => {
-            handleEditCustomer(selectedCustomerId, values, formikHelpers);
-          }}
-        />
+      {isRenderDone && (
+        <>
+          <EditCustomerForm />
+          <CustomerForm
+            titleText="EDIT CUSTOMER"
+            submitButtonText="UPDATE CUSTOMER"
+            onSubmit={async (
+              values: CustomerFormValues,
+              formikHelpers: FormikHelpers<CustomerFormValues>
+            ) => {
+              await handleEditCustomer(
+                selectedCustomerId,
+                values,
+                formikHelpers
+              );
+              setSelectedCustomerId("");
+            }}
+          />
+        </>
       )}
     </div>
   );
