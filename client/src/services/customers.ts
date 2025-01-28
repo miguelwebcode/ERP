d0aeecbd5da406ccbc44e07b9687df8347f2ccf3
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 21:33:13 +0100

    Create customer delete function

--- client/src/services/customers.ts
+++ client/src/services/customers.ts
@@ -6,6 +6,7 @@ import {
   where,
   updateDoc,
   doc,
+  deleteDoc,
 } from "firebase/firestore";
 import { auth, db } from "../firebaseConfig";
 import { v4 as uuidv4 } from "uuid";
@@ -125,5 +126,32 @@ export const handleEditCustomer = async (
   } catch (error) {
     console.error("Error updating customer: ", error);
     alert("Error updating customer!");
+  }
+};
+
+export const deleteCustomerById = async (customerId: string) => {
+  const user = auth.currentUser;
+  if (!user) {
+    console.error("User not authenticated. Cannot delete from Firestore.");
+    return;
+  }
+
+  const customersCollection = collection(db, "customers");
+  const q = query(customersCollection, where("customerId", "==", customerId));
+
+  try {
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      console.log("No matching documents.");
+      return null;
+    }
+
+    const documentId = querySnapshot.docs[0].id;
+    const customerDocRef = doc(db, "customers", documentId);
+
+    await deleteDoc(customerDocRef);
+    alert("Customer deleted successfully!");
+  } catch (error) {
+    console.error("Error deleting customer: ", error);
   }
 };
