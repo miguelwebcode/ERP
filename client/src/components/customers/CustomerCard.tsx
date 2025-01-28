bd50e888af8719bbc532ad5dc06e6d7e7a595ed2
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 21:21:23 +0100

    Create customer component

---
+++ client/src/components/customers/CustomerCard.tsx
@@ -0,0 +1,48 @@
+import { Customer } from "../../types";
+
+type CustomerCardProps = {
+  customer: Customer;
+};
+export const CustomerCard = ({ customer }: CustomerCardProps) => {
+  return (
+    <div className="flex flex-col bg-slate-50 rounded-xl shadow-lg m-4 p-4 w-fit h-fit">
+      <div className="flex mb-4 gap-1 items-center">
+        <label className="block text-sm font-medium">Name:</label>
+        <span className="text-md">{customer.name}</span>
+      </div>
+      <div className="flex mb-4  gap-1 items-center">
+        <label className="block text-sm font-medium">Email:</label>
+        <span>{customer.email}</span>
+      </div>
+
+      <div className="flex mb-4  gap-1 items-center">
+        <label className="block text-sm font-medium">Phone::</label>
+        <span className="text-md">{customer.phone}</span>
+      </div>
+      <div className="flex mb-4  gap-1 items-center">
+        <label className="block text-sm font-medium">Address:</label>
+        <span>{customer.company}</span>
+      </div>
+
+      <div className="flex mb-4  gap-1 items-center">
+        <label className="block text-sm font-medium">Company:</label>
+        <span className="text-md">{customer.company}</span>
+      </div>
+      <div className="flex mb-4  gap-1 items-center">
+        <label className="block text-sm font-medium">Project:</label>
+        <span>{customer.project}</span>
+      </div>
+
+      <div className="flex flex-col mb-4 gap-1">
+        <label className="block text-sm font-medium">Created at:</label>
+        <span className="text-md">{customer.createdAt}</span>
+      </div>
+      {customer.updatedAt && (
+        <div className="flex flex-col mb-4 gap-1">
+          <label className="block text-sm font-medium">Updated at:</label>
+          <span>{customer.updatedAt}</span>
+        </div>
+      )}
+    </div>
+  );
+};
