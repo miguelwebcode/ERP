72a35c07c924c614f409e22672ce5ed9fde27bc3
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 21:21:34 +0100

    Generate CustomerCards

--- client/src/views/ReadCustomersView.tsx
+++ client/src/views/ReadCustomersView.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+import { getAllCustomers } from "../services/customers";
+import { Customer } from "../types";
+import { CustomerCard } from "../components/customers/CustomerCard";
+
 export const ReadCustomersView = () => {
-  return <div>ReadCustomersView</div>;
+  const [customers, setCustomers] = useState<Customer[]>([]);
+  useEffect(() => {
+    const fetchAllCustomers = async () => {
+      const result = await getAllCustomers();
+      setCustomers(result as Customer[]);
+    };
+    fetchAllCustomers();
+  }, []);
+  return (
+    <div className="flex flex-wrap">
+      {customers.map((customer) => {
+        return <CustomerCard customer={customer} />;
+      })}
+    </div>
+  );
 };
