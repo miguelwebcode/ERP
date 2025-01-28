cfe647e6817d29b02a76b92c982699a1dd36a982
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-28 20:16:43 +0100

    Add routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -9,6 +9,10 @@ import { AddCustomerView } from "./views/AddCustomerView";
 import { EditCustomerView } from "./views/EditCustomerView";
 import { AddProjectView } from "./views/AddProjectView";
 import { EditProjectView } from "./views/EditProjectView";
+import { ReadCustomersView } from "./views/ReadCustomersView";
+import { DeleteCustomerView } from "./views/DeleteCustomerView";
+import { ReadProjectsView } from "./views/ReadProjectsView";
+import { DeleteProjectView } from "./views/DeleteProjectView";
 
 export default function AppRouter() {
   return (
@@ -20,10 +24,14 @@ export default function AppRouter() {
           <Route path="/register" element={<RegisterView />} />
           <Route path="/customers" element={<CustomersView />} />
           <Route path="/customers/add" element={<AddCustomerView />} />
+          <Route path="/customers/read" element={<ReadCustomersView />} />
           <Route path="/customers/edit" element={<EditCustomerView />} />
+          <Route path="/customers/delete" element={<DeleteCustomerView />} />
           <Route path="/projects" element={<ProjectsView />} />
           <Route path="/projects/add" element={<AddProjectView />} />
+          <Route path="/projects/read" element={<ReadProjectsView />} />
           <Route path="/projects/edit" element={<EditProjectView />} />
+          <Route path="/projects/delete" element={<DeleteProjectView />} />
         </Route>
       </Routes>
     </BrowserRouter>
