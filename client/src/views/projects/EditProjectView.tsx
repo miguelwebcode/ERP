bc7cf39aab006c5b4470d1abb69ab99f8d7ad97e
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 12:18:43 +0100

    Modify edit project view

--- client/src/views/projects/EditProjectView.tsx
+++ client/src/views/projects/EditProjectView.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+import { useAppStore } from "../../stores/app-store";
+import { EditProjectForm } from "../../components/projects/EditProjectForm";
+import ProjectForm from "../../components/projects/ProjectForm";
+import { ProjectFormValues } from "../../types/form-values-types";
+import { FormikHelpers } from "formik";
+import { handleEditProject } from "../../services/projects";
+
 export const EditProjectView = () => {
+  const [isRenderDone, setIsRenderDone] = useState(false);
+  const selectedProjectId = useAppStore((state) => state.selectedProjectId);
+
+  const setSelectedProjectId = useAppStore(
+    (state) => state.setSelectedProjectId
+  );
+
+  useEffect(() => {
+    setSelectedProjectId("");
+    setIsRenderDone(true);
+  }, []);
   return (
     <div className="flex flex-col md:flex-row justify-center px-5">
-      EditProjectView
+      {isRenderDone && (
+        <>
+          <EditProjectForm />
+          <ProjectForm
+            titleText="EDIT PROJECT"
+            submitButtonText="UPDATE PROJECT"
+            canBeDisabled={true}
+            onSubmit={async (
+              values: ProjectFormValues,
+              formikHelpers: FormikHelpers<ProjectFormValues>
+            ) => {
+              await handleEditProject(selectedProjectId, values, formikHelpers);
+              setSelectedProjectId("");
+            }}
+          />
+        </>
+      )}
     </div>
   );
 };
