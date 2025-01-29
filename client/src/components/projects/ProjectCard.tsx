4185fee7deb20ebcce155ab07abcefc1dc270587
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 17:28:37 +0100

    Modify project card

--- client/src/components/projects/ProjectCard.tsx
+++ client/src/components/projects/ProjectCard.tsx
@@ -5,34 +5,34 @@ type ProjectCardProps = {
 };
 export const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    <div className="flex flex-col bg-slate-50 rounded-xl shadow-lg m-4 p-4 w-fit h-fit">
-      <div className="flex mb-4 gap-1 items-center">
+    <>
+      <div className="flex flex-col mb-4 gap-1 ">
         <label className="block text-sm font-medium">Name:</label>
         <span className="text-md">{project.name}</span>
       </div>
-      <div className="flex mb-4  gap-1 items-center">
+      <div className="flex flex-col mb-4  gap-1 ">
         <label className="block text-sm font-medium">Description:</label>
         <span>{project.description}</span>
       </div>
 
-      <div className="flex mb-4  gap-1 items-center">
+      <div className="flex flex-col mb-4  gap-1 ">
         <label className="block text-sm font-medium">Customer ID:</label>
         <span className="text-md">{project.customerId}</span>
       </div>
-      <div className="flex mb-4  gap-1 items-center">
+      <div className="flex flex-col mb-4  gap-1 ">
         <label className="block text-sm font-medium">Start Date:</label>
         <span>{project.startDate}</span>
       </div>
 
-      <div className="flex mb-4  gap-1 items-center">
+      <div className="flex flex-col mb-4  gap-1 ">
         <label className="block text-sm font-medium">End Date:</label>
         <span className="text-md">{project.endDate}</span>
       </div>
-      <div className="flex mb-4  gap-1 items-center">
+      <div className="flex flex-col mb-4  gap-1 ">
         <label className="block text-sm font-medium">State:</label>
         <span>{project.state}</span>
       </div>
-      <div className="flex mb-4  gap-1 items-center">
+      <div className="flex flex-col mb-4  gap-1 ">
         <label className="block text-sm font-medium">Developer:</label>
         <span>{project.developer}</span>
       </div>
@@ -46,6 +46,6 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
           <span>{project.updatedAt}</span>
         </div>
       )}
-    </div>
+    </>
   );
 };
