c5ebb512cfa93849286d2b18e830106b2781874f
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 19:30:29 +0100

    Create format date function

---
+++ client/src/services/index.ts
@@ -0,0 +1,12 @@
+export const formatDate = (date: Date) => {
+  const padTo2Digits = (num: number) => num.toString().padStart(2, "0");
+
+  return (
+    `${padTo2Digits(date.getDate())}/` +
+    `${padTo2Digits(date.getMonth() + 1)}/` +
+    `${date.getFullYear()} ` +
+    `${padTo2Digits(date.getHours())}:` +
+    `${padTo2Digits(date.getMinutes())}:` +
+    `${padTo2Digits(date.getSeconds())}`
+  );
+};
