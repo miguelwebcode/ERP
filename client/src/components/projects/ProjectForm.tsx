67feedb5ca87432775cc8d7d2f0f89625e1c5f17
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 20:04:46 +0100

    Add margin top

--- client/src/components/projects/ProjectForm.tsx
+++ client/src/components/projects/ProjectForm.tsx
@@ -146,7 +146,7 @@ const ProjectForm = ({
         </div>
         <button
           type="submit"
-          className="w-3/5 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 uppercase font-bold disabled:bg-gray-500"
+          className="w-3/5 bg-blue-500 text-white mt-4 py-2 px-4 rounded hover:bg-blue-600 uppercase font-bold disabled:bg-gray-500"
           disabled={fieldDisabled}
         >
           {submitButtonText}
