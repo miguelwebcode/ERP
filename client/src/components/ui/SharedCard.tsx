f29fe5539ca01ad8518bbd8d70395bf22b8ed164
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 17:29:01 +0100

    Create shared card

---
+++ client/src/components/ui/SharedCard.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+type SharedCard = {
+  children: ReactNode;
+};
+export const SharedCard = ({ children }: SharedCard) => {
+  return (
+    <div className="flex flex-col bg-slate-50 rounded-xl shadow-lg p-4 w-fit h-fit">
+      {children}
+    </div>
+  );
+};
