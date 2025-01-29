d687c86b8f7fc36a3d65b01660f5587f1053e1b3
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 18:41:00 +0100

    Pass props

--- client/src/views/customers/EditCustomerView.tsx
+++ client/src/views/customers/EditCustomerView.tsx
@@ -1,10 +1,13 @@
 import { FormikHelpers } from "formik";
 import CustomerForm from "../../components/customers/CustomerForm";
-import EditCustomerForm from "../../components/customers/EditCustomerForm";
 import { handleEditCustomer } from "../../services/customers";
 import { useAppStore } from "../../stores/app-store";
-import { CustomerFormValues } from "../../types/form-values-types";
+import {
+  CustomerFormValues,
+  SelectCustomerFormValues,
+} from "../../types/form-values-types";
 import { useState, useEffect } from "react";
+import SelectCustomerForm from "../../components/customers/DeleteCustomerForm";
 
 export const EditCustomerView = () => {
   const [isRenderDone, setIsRenderDone] = useState(false);
@@ -18,11 +21,27 @@ export const EditCustomerView = () => {
     setSelectedCustomerId("");
     setIsRenderDone(true);
   }, []);
+
+  const handleSubmit = async (
+    values: SelectCustomerFormValues,
+    formikHelpers: FormikHelpers<SelectCustomerFormValues>
+  ) => {
+    try {
+      setSelectedCustomerId(values.customerId);
+      formikHelpers.resetForm();
+    } catch (error) {
+      console.error("Error getting customer: ", error);
+      alert("Error getting customer!");
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row justify-center px-5">
       {isRenderDone && (
         <>
-          <EditCustomerForm />
+          <SelectCustomerForm
+            buttonText="FETCH CUSTOMER"
+            onSubmit={handleSubmit}
+          />
           <CustomerForm
             titleText="EDIT CUSTOMER"
             submitButtonText="UPDATE CUSTOMER"
