8fa0cef599e57a3d00e687f87d2b6eaa0a1f3797
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 19:46:09 +0100

    Improve Card UI

--- client/src/components/projects/ProjectCard.tsx
+++ client/src/components/projects/ProjectCard.tsx
@@ -7,17 +7,23 @@ type ProjectCardProps = {
 export const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
     <>
-      <CardField label="Name" value={project.name} />
-      <CardField label="Description" value={project.description} />
-      <CardField label="Customer ID" value={project.customerId} />
-      <CardField label="Start Date" value={project.startDate} />
-      <CardField label="End Date" value={project.endDate} />
-      <CardField label="State" value={project.state} />
-      <CardField label="Developer" value={project.developer} />
-      <CardField label="Created at" value={project.createdAt} />
-      {project.updatedAt && (
-        <CardField label="Updated at" value={project.updatedAt} />
-      )}
+      <div className="flex gap-10">
+        <div className="flex flex-col">
+          <CardField label="Name" value={project.name} />
+          <CardField label="Description" value={project.description} />
+          <CardField label="Customer ID" value={project.customerId} />
+          <CardField label="Developer" value={project.developer} />
+        </div>
+        <div className="flex flex-col">
+          <CardField label="State" value={project.state} />
+          <CardField label="Start Date" value={project.startDate} />
+          <CardField label="End Date" value={project.endDate} />
+          <CardField label="Created at" value={project.createdAt} />
+          {project.updatedAt && (
+            <CardField label="Updated at" value={project.updatedAt} />
+          )}
+        </div>
+      </div>
     </>
   );
 };
