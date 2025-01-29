64851ef49d6d07a43f32f90a4ff158e58be6bc4e
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 19:14:06 +0100

    Create card field component

---
+++ client/src/components/ui/CardField.tsx
@@ -0,0 +1,13 @@
+type CardFieldProps = {
+  label: string;
+  value: string;
+};
+
+export const CardField = ({ label, value }: CardFieldProps) => {
+  return (
+    <div className="flex flex-col mb-4 gap-1 ">
+      <label className="block text-sm font-medium">{label}</label>
+      <span className="text-md">{value}</span>
+    </div>
+  );
+};
