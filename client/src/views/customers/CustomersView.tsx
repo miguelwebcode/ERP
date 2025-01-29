78ca69c7b84850216128792f9dc6317612795160
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 18:00:05 +0100

    Make all buttons equal size

--- client/src/views/customers/CustomersView.tsx
+++ client/src/views/customers/CustomersView.tsx
@@ -7,31 +7,32 @@ export const CustomersView = () => {
     <div className="flex justify-center">
       <div className="flex flex-col items-center justify-center bg-white p-6 rounded shadow-md gap-4">
         <h1 className="uppercase font-bold text-xl">Customer's VIEW</h1>
-
-        <SharedButton
-          text="CREATE Customer"
-          handleClick={() => {
-            navigate("/customers/add");
-          }}
-        />
-        <SharedButton
-          text="READ Customers"
-          handleClick={() => {
-            navigate("/customers/read");
-          }}
-        />
-        <SharedButton
-          text="UPDATE Customer"
-          handleClick={() => {
-            navigate("/customers/edit");
-          }}
-        />
-        <SharedButton
-          text="DELETE Customer"
-          handleClick={() => {
-            navigate("/customers/delete");
-          }}
-        />
+        <div className="flex flex-col gap-3">
+          <SharedButton
+            text="CREATE Customer"
+            handleClick={() => {
+              navigate("/customers/add");
+            }}
+          />
+          <SharedButton
+            text="READ Customers"
+            handleClick={() => {
+              navigate("/customers/read");
+            }}
+          />
+          <SharedButton
+            text="UPDATE Customer"
+            handleClick={() => {
+              navigate("/customers/edit");
+            }}
+          />
+          <SharedButton
+            text="DELETE Customer"
+            handleClick={() => {
+              navigate("/customers/delete");
+            }}
+          />
+        </div>
       </div>
     </div>
   );
--- client/src/views/projects/ProjectsView.tsx
+++ client/src/views/projects/ProjectsView.tsx
@@ -8,30 +8,32 @@ export const ProjectsView = () => {
     <div className="flex justify-center">
       <div className="flex flex-col items-center justify-center bg-white p-6 rounded shadow-md gap-4">
         <h1 className="uppercase font-bold text-xl">Project's VIEW</h1>
-        <SharedButton
-          text="CREATE Project"
-          handleClick={() => {
-            navigate("/projects/add");
-          }}
-        />
-        <SharedButton
-          text="READ Projects"
-          handleClick={() => {
-            navigate("/projects/read");
-          }}
-        />
-        <SharedButton
-          text="UPDATE Project"
-          handleClick={() => {
-            navigate("/projects/edit");
-          }}
-        />
-        <SharedButton
-          text="DELETE Project"
-          handleClick={() => {
-            navigate("/projects/delete");
-          }}
-        />
+        <div className="flex flex-col gap-3">
+          <SharedButton
+            text="CREATE Project"
+            handleClick={() => {
+              navigate("/projects/add");
+            }}
+          />
+          <SharedButton
+            text="READ Projects"
+            handleClick={() => {
+              navigate("/projects/read");
+            }}
+          />
+          <SharedButton
+            text="UPDATE Project"
+            handleClick={() => {
+              navigate("/projects/edit");
+            }}
+          />
+          <SharedButton
+            text="DELETE Project"
+            handleClick={() => {
+              navigate("/projects/delete");
+            }}
+          />
+        </div>
       </div>
     </div>
   );
