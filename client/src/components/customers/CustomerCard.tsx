877e632311a9f2c8dd6599151650ec828da51a16
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 19:41:12 +0100

    Modify customer card

--- client/src/components/customers/CustomerCard.tsx
+++ client/src/components/customers/CustomerCard.tsx
@@ -7,16 +7,22 @@ type CustomerCardProps = {
 export const CustomerCard = ({ customer }: CustomerCardProps) => {
   return (
     <>
-      <CardField label="Name" value={customer.name} />
-      <CardField label="Email" value={customer.email} />
-      <CardField label="Phone" value={customer.phone} />
-      <CardField label="Address" value={customer.address} />
-      <CardField label="Company" value={customer.company} />
-      <CardField label="Project" value={customer.project} />
-      <CardField label="Created at" value={customer.createdAt} />
-      {customer.updatedAt && (
-        <CardField label="Updated at" value={customer.updatedAt} />
-      )}
+      <div className="flex gap-10">
+        <div className="flex flex-col">
+          <CardField label="Name" value={customer.name} />
+          <CardField label="Email" value={customer.email} />
+          <CardField label="Phone" value={customer.phone} />
+          <CardField label="Address" value={customer.address} />
+        </div>
+        <div className="flex flex-col">
+          <CardField label="Company" value={customer.company} />
+          <CardField label="Project" value={customer.project} />
+          <CardField label="Created at" value={customer.createdAt} />
+          {customer.updatedAt && (
+            <CardField label="Updated at" value={customer.updatedAt} />
+          )}
+        </div>
+      </div>
     </>
   );
 };
