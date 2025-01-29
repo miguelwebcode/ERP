aff7c21dfe37099690eed653e6633fe2bddb0f6c
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 18:18:47 +0100

    Pass props to component

--- client/src/views/projects/DeleteProjectView.tsx
+++ client/src/views/projects/DeleteProjectView.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useRef, useState } from "react";
-import DeleteProjectForm from "../../components/projects/DeleteProjectForm";
+import SelectProjectForm from "../../components/projects/DeleteProjectForm";
 import { useAppStore } from "../../stores/app-store";
 import { deleteProjectById, getProjectById } from "../../services/projects";
 import { Project } from "../../types";
 import { SharedCard } from "../../components/ui/SharedCard";
 import { ProjectCard } from "../../components/projects/ProjectCard";
 import { SharedButton } from "../../components/ui/SharedButton";
+import { SelectProjectFormValues } from "../../types/form-values-types";
+import { FormikHelpers } from "formik";
 
 export const DeleteProjectView = () => {
   const selectedProjectId = useAppStore((state) => state.selectedProjectId);
@@ -33,10 +35,23 @@ export const DeleteProjectView = () => {
       fetchProject();
     }
   }, [selectedProjectId]);
+
+  const handleSubmit = async (
+    values: SelectProjectFormValues,
+    formikHelpers: FormikHelpers<SelectProjectFormValues>
+  ) => {
+    try {
+      setSelectedProjectId(values.projectId);
+      formikHelpers.resetForm();
+    } catch (error) {
+      console.error("Error getting project: ", error);
+      alert("Error getting project!");
+    }
+  };
   return (
     <div className="flex flex-col md:flex-row justify-center px-5">
       <>
-        <DeleteProjectForm />
+        <SelectProjectForm buttonText="FETCH PROJECT" onSubmit={handleSubmit} />
         {selectedProjectId && (
           <SharedCard>
             <ProjectCard project={selectedProject} />
