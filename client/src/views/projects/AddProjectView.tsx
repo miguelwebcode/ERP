633e7c30f9fcdafe903d16c14caa5fc4f282547b
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 12:18:33 +0100

    Modify view's logic

--- client/src/views/projects/AddProjectView.tsx
+++ client/src/views/projects/AddProjectView.tsx
@@ -1,14 +1,28 @@
+import { useState, useEffect } from "react";
 import ProjectForm from "../../components/projects/ProjectForm";
 import { handleCreateProject } from "../../services/projects";
+import { useAppStore } from "../../stores/app-store";
 
 export const AddProjectView = () => {
+  const [isRenderDone, setIsRenderDone] = useState(false);
+
+  const setSelectedProjectId = useAppStore(
+    (state) => state.setSelectedProjectId
+  );
+
+  useEffect(() => {
+    setSelectedProjectId("");
+    setIsRenderDone(true);
+  });
   return (
     <div className="flex justify-center">
-      <ProjectForm
-        titleText="NEW PROJECT"
-        submitButtonText="CREATE PROJECT"
-        onSubmit={handleCreateProject}
-      />
+      {isRenderDone && (
+        <ProjectForm
+          titleText="NEW PROJECT"
+          submitButtonText="CREATE PROJECT"
+          onSubmit={handleCreateProject}
+        />
+      )}
     </div>
   );
 };
