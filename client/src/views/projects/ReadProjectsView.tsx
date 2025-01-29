f83e0fab49e47ff36256cc0125fa9739b12f9232
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 12:18:04 +0100

    Create read projects view

--- client/src/views/projects/ReadProjectsView.tsx
+++ client/src/views/projects/ReadProjectsView.tsx
@@ -1,3 +1,22 @@
+import { useState, useEffect } from "react";
+import { Project } from "../../types";
+import { getAllProjects } from "../../services/projects";
+import { ProjectCard } from "../../components/projects/ProjectCard";
+
 export const ReadProjectsView = () => {
-  return <div className="flex justify-center flex-wrap">ReadProjectsView</div>;
+  const [projects, setProjects] = useState<Project[]>([]);
+  useEffect(() => {
+    const fetchAllprojects = async () => {
+      const result = await getAllProjects();
+      setProjects(result as Project[]);
+    };
+    fetchAllprojects();
+  }, []);
+  return (
+    <div className="flex justify-center flex-wrap">
+      {projects.map((project) => {
+        return <ProjectCard project={project} />;
+      })}
+    </div>
+  );
 };
