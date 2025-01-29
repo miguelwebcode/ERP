18a68b1747df096a7f603702a7c2847b3d045959
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-29 11:18:42 +0100

    Add state and action

--- client/src/stores/app-store.ts
+++ client/src/stores/app-store.ts
@@ -5,11 +5,13 @@ import { devtools } from "zustand/middleware";
 export type AppState = {
   user: User | null;
   selectedCustomerId: string;
+  selectedProjectId: string;
 };
 
 export type AppActions = {
   setUser: (user: User | null) => void;
   setSelectedCustomerId: (selectedCustomerId: string) => void;
+  setSelectedProjectId: (selectedProjectId: string) => void;
 };
 
 export type AppStore = AppState & AppActions;
@@ -29,6 +31,9 @@ export const useAppStore = create<AppStore>()(
     },
     setSelectedCustomerId: (selectedCustomerId) => {
       set({ selectedCustomerId });
+    },
+    setSelectedProjectId: (selectedProjectId) => {
+      set({ selectedProjectId });
     },
   }))
 );
