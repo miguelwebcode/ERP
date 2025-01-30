1651787e9c04620c089fc71f41524d3e17cabd2a
miguelwebcode <miguelcodeonline@gmail.com>
2025-01-30 21:12:17 +0100

    Allow .jest sufix

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -2,4 +2,7 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: [
+    "**/?(*.)+(jest.test).[jt]s?(x)", // Busca archivos con sufijo .jest.test.ts o .jest.test.tsx
+  ],
 };
