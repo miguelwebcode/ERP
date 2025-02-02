f9942a946b0e7576baa7450851b754944058289f
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-02 18:06:47 +0100

    Config vitest defineConfig file

---
+++ client/vitestConfig.ts
@@ -0,0 +1,36 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+export default defineConfig({
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [react(), viteTsConfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    // Yo no uso redux, que me hace falta de aqui?
+    alias: [
+      {
+        find: "react-redux/es/exports",
+        replacement: path.resolve(
+          __dirname,
+          "./node_modules/react-redux/lib/exports"
+        ),
+      },
+    ],
+  },
+  server: {
+    // this ensures that the browser openss upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
+});
