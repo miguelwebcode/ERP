b6f4e695552463724e24338b9f8aa44be9e7960d
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-03 19:36:13 +0100

    Comment fake vitest setup files

--- client/src/setupTests.ts
+++ client/src/setupTests.ts
@@ -1,29 +1,29 @@
-import { beforeAll, afterEach } from "vitest";
-import { cleanup } from "@testing-library/react";
-import {
-  RulesTestEnvironment,
-  initializeTestEnvironment,
-} from "@firebase/rules-unit-testing";
-import { readFileSync } from "fs";
+// import { beforeAll, afterEach } from "vitest";
+// import { cleanup } from "@testing-library/react";
+// import {
+//   RulesTestEnvironment,
+//   initializeTestEnvironment,
+// } from "@firebase/rules-unit-testing";
+// import { readFileSync } from "fs";
 
-// Luego donde se usa esta variable? Me falta algo más en otro archivo?
-export let testEnv: RulesTestEnvironment | null = null;
+// // Luego donde se usa esta variable? Me falta algo más en otro archivo?
+// export let testEnv: RulesTestEnvironment | null = null;
 
-beforeAll(async () => {
-  // Se usa testEnv en vez de db en los tests?
-  testEnv = await initializeTestEnvironment({
-    projectId: "erp-fire",
-    hub: {
-      host: "127.0.0.1",
-      // Es el PORT que toca en mi caso? Tengo que consultar mi firebase.json?
-      port: 4000,
-    },
-    firestore: {
-      rules: readFileSync("firestore.rules", "utf8"),
-    },
-  });
-});
+// beforeAll(async () => {
+//   // Se usa testEnv en vez de db en los tests?
+//   testEnv = await initializeTestEnvironment({
+//     projectId: "erp-fire",
+//     hub: {
+//       host: "127.0.0.1",
+//       // Es el PORT que toca en mi caso? Tengo que consultar mi firebase.json?
+//       port: 4000,
+//     },
+//     firestore: {
+//       rules: readFileSync("firestore.rules", "utf8"),
+//     },
+//   });
+// });
 
-afterEach(() => {
-  cleanup();
-});
+// afterEach(() => {
+//   cleanup();
+// });
--- client/vitestConfig.ts
+++ client/vitestConfig.ts
@@ -1,38 +1,38 @@
-/// <reference types="vitest" />
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
-import path from "path";
+// /// <reference types="vitest" />
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import viteTsConfigPaths from "vite-tsconfig-paths";
+// import path from "path";
 
-// Que nombre le doy a este archivo? está en el lugar correcto? (/client/viteConfig.ts)
-// Respuesta, este archivo deberia ser vitest.config.ts
-export default defineConfig({
-  base: "/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  plugins: [react(), viteTsConfigPaths()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-    // Yo no uso redux, que me hace falta de aqui?
-    alias: [
-      {
-        find: "react-redux/es/exports",
-        replacement: path.resolve(
-          __dirname,
-          "./node_modules/react-redux/lib/exports"
-        ),
-      },
-    ],
-  },
-  server: {
-    // this ensures that the browser openss upon server start
-    open: true,
-    // this sets a default port to 3000
-    port: 3000,
-  },
-});
+// // Que nombre le doy a este archivo? está en el lugar correcto? (/client/viteConfig.ts)
+// // Respuesta, este archivo deberia ser vitest.config.ts
+// export default defineConfig({
+//   base: "/",
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+//   plugins: [react(), viteTsConfigPaths()],
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//     setupFiles: "./src/setupTests.ts",
+//     // Yo no uso redux, que me hace falta de aqui?
+//     alias: [
+//       {
+//         find: "react-redux/es/exports",
+//         replacement: path.resolve(
+//           __dirname,
+//           "./node_modules/react-redux/lib/exports"
+//         ),
+//       },
+//     ],
+//   },
+//   server: {
+//     // this ensures that the browser openss upon server start
+//     open: true,
+//     // this sets a default port to 3000
+//     port: 3000,
+//   },
+// });
