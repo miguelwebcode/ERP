e7a7d16e8a028fe916dcd9ca6787aaef8dc62d59
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-03 19:34:52 +0100

    CustomerCard test

---
+++ client/src/components/customers/CustomerCard/CustomerCard.vitest.test.ts
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import { CustomerCard } from "./CustomerCard";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom";
+
+describe("CustomerCard", () => {
+  it("renders all customer data", () => {});
+});
