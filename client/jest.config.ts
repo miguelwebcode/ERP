4efc773360749496c7e610bb6151c984c8a2c569
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-03 19:42:15 +0100

    Use jsdom for jest config

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: [
     "**/?(*.)+(jest.test).[jt]s?(x)", // Busca archivos con sufijo .jest.test.ts o .jest.test.tsx
