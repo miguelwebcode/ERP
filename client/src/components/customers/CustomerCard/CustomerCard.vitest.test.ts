ac272a112ad671bd319fd81722226dec25e1f9bc
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-03 20:17:35 +0100

    Create CustomerCard test

--- client/src/components/customers/CustomerCard/CustomerCard.vitest.test.ts
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { CustomerCard } from "./CustomerCard";
-import { describe, it, expect } from "vitest";
-import "@testing-library/jest-dom";
-
-describe("CustomerCard", () => {
-  it("renders all customer data", () => {});
-});
---
+++ client/src/components/customers/CustomerCard/CustomerCard.vitest.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from "@testing-library/react";
+import { CustomerCard } from "./CustomerCard";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom";
+import { Customer } from "../../../types";
+
+describe("CustomerCard", () => {
+  it("renders all customer data except updatedAt", () => {
+    const customer: Customer = {
+      address: "street",
+      company: "Corp",
+      customerId: "CUST123456",
+      email: "john.doe@techcorp.com",
+      name: "name",
+      phone: "+1-555-1234",
+      project: "Project Alpha",
+      createdAt: "2025-02-02 13:24",
+      updatedAt: undefined,
+    };
+
+    render(<CustomerCard customer={customer} />);
+
+    expect(screen.getByText(/street/i)).toBeInTheDocument();
+    expect(screen.getByText("Corp")).toBeInTheDocument();
+    expect(screen.getByText(/john.doe/i)).toBeInTheDocument();
+    expect(screen.getByText("name")).toBeInTheDocument();
+    expect(screen.getByText("+1-555-1234")).toBeInTheDocument();
+    expect(screen.getByText("Project Alpha")).toBeInTheDocument();
+    expect(screen.getByText("2025-02-02 13:24")).toBeInTheDocument();
+    expect(screen.queryByText(/updated at/i)).not.toBeInTheDocument();
+  });
+  it("renders all customer data AND updatedAt", () => {
+    const customer: Customer = {
+      address: "street",
+      company: "Corp",
+      customerId: "CUST123456",
+      email: "john.doe@techcorp.com",
+      name: "name",
+      phone: "+1-555-1234",
+      project: "Project Alpha",
+      createdAt: "2025-02-02 13:24",
+      updatedAt: "2025-02-03 23:24",
+    };
+
+    render(<CustomerCard customer={customer} />);
+
+    expect(screen.getByText(/street/i)).toBeInTheDocument();
+    expect(screen.getByText("Corp")).toBeInTheDocument();
+    expect(screen.getByText(/john.doe/i)).toBeInTheDocument();
+    expect(screen.getByText("name")).toBeInTheDocument();
+    expect(screen.getByText("+1-555-1234")).toBeInTheDocument();
+    expect(screen.getByText("Project Alpha")).toBeInTheDocument();
+    expect(screen.getByText("2025-02-02 13:24")).toBeInTheDocument();
+    expect(screen.queryByText(/updated at/i)).toBeInTheDocument();
+  });
+});
