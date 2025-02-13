8cd999be5badbb0eb82c7b028a0583448b8b1584
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-13 17:31:05 +0100

    Implement logic

--- client/src/views/RegisterView.tsx
+++ client/src/views/RegisterView.tsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import RegisterForm from "../components/register/RegisterForm/RegisterForm";
+import { watchAuthState } from "../services/auth/auth";
+import { useAppStore } from "../stores/app-store";
 
 export const RegisterView = () => {
+  const navigate = useNavigate();
+  const user = useAppStore((state) => state.user);
+  const setUser = useAppStore((state) => state.setUser);
+
+  useEffect(() => {
+    watchAuthState(setUser);
+    if (user != null) {
+      navigate("/");
+    }
+  }, [user]);
   return <RegisterForm />;
 };
