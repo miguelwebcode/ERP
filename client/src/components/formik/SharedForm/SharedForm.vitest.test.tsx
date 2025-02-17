c30f79219f416ec2ff408a061e35396ba28cbf40
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-17 18:16:07 +0100

    Move LoginForm test suite to its own file

--- client/src/components/formik/SharedForm/SharedForm.vitest.test.tsx
+++ client/src/components/formik/SharedForm/SharedForm.vitest.test.tsx
@@ -21,66 +21,6 @@ import { CustomInput } from "../CustomInput/CustomInput";
 import { CustomSelect } from "../CustomSelect/CustomSelect";
 
 describe("SharedForm", () => {
-  describe("SharedForm wrapping LoginFormValues", () => {
-    vi.mock("react-router-dom", () => ({
-      useNavigate: vi.fn(),
-    }));
-    const mockOnSubmit = vi.fn();
-    const initialValues: LoginFormValues = {
-      email: "em@ail.com",
-      password: "111222",
-    };
-    beforeEach(() => {
-      render(
-        <SharedForm<LoginFormValues>
-          initialValues={initialValues}
-          validationSchema={loginFormValidationSchema}
-          onSubmit={mockOnSubmit}
-        >
-          <CustomInput
-            label="Email"
-            name="email"
-            type="text"
-            placeholder="Enter your email"
-          />
-          <CustomInput
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="Enter your password"
-          />
-          <button
-            type="submit"
-            className="w-4/5 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-          >
-            Login
-          </button>
-        </SharedForm>
-      );
-    });
-    it("shows correct initialValues", () => {
-      const inputEmail = screen.getByLabelText("Email") as HTMLInputElement;
-      expect(inputEmail).toBeInTheDocument();
-      expect(inputEmail.type).toBe("text");
-      expect(inputEmail.value).toBe(initialValues.email);
-
-      const inputPassword = screen.getByLabelText(
-        "Password"
-      ) as HTMLInputElement;
-      expect(inputPassword).toBeInTheDocument();
-      expect(inputPassword.type).toBe("password");
-      expect(inputPassword.value).toBe(initialValues.password);
-    });
-    it("calls onSubmit when form filled out and button clicked", async () => {
-      const button = screen.getByRole("button", { name: /login/i });
-      expect(button).toBeInTheDocument();
-      fireEvent.click(button);
-
-      await waitFor(() => {
-        expect(mockOnSubmit).toHaveBeenCalled();
-      });
-    });
-  });
   describe("SharedForm wrapping RegisterFormValues", () => {
     const initialValues: RegisterFormValues = {
       name: "name",
