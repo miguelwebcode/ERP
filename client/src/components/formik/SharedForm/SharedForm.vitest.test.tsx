4ff0286dd7f1f8d3b7c23e96673de41b09a93938
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-17 18:09:18 +0100

    Move SelectCustomerForm test suite to its own file

--- client/src/components/formik/SharedForm/SharedForm.vitest.test.tsx
+++ client/src/components/formik/SharedForm/SharedForm.vitest.test.tsx
@@ -21,61 +21,6 @@ import { CustomInput } from "../CustomInput/CustomInput";
 import { CustomSelect } from "../CustomSelect/CustomSelect";
 
 describe("SharedForm", () => {
-  describe("SharedForm wrapping SelectCustomerFormValues", () => {
-    const initialValues: SelectCustomerFormValues = {
-      customerId: "1",
-    };
-    const mockOnSubmit = vi.fn();
-    const customerIds = ["1", "2", "3"];
-    beforeEach(() => {
-      render(
-        <SharedForm<SelectCustomerFormValues>
-          initialValues={initialValues}
-          validationSchema={selectCustomerFormValidationSchema}
-          onSubmit={mockOnSubmit}
-        >
-          <CustomSelect label="Customer ID" name="customerId">
-            <option value="" className="text-center">
-              -- Select customer ID --
-            </option>
-            {customerIds.map((id, index) => (
-              <option key={index} value={id}>
-                {id}
-              </option>
-            ))}
-          </CustomSelect>
-          <button
-            type="submit"
-            className="w-4/5 bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600"
-          >
-            Submit
-          </button>
-        </SharedForm>
-      );
-    });
-    it("shows correct initialValues", () => {
-      const selectCustomerId = screen.getByLabelText(
-        "Customer ID"
-      ) as HTMLSelectElement;
-      expect(selectCustomerId).toBeInTheDocument();
-      expect(selectCustomerId.value).toBe("1");
-    });
-    it("calls onSubmit when: option selected and button is clicked", async () => {
-      const selectCustomerId = screen.getByLabelText(
-        "Customer ID"
-      ) as HTMLSelectElement;
-      expect(selectCustomerId).toBeInTheDocument();
-      expect(selectCustomerId.value).toBe("1");
-
-      const button = screen.getByRole("button", { name: /subm/i });
-      expect(button).toBeInTheDocument();
-      fireEvent.click(button);
-
-      await waitFor(() => {
-        expect(mockOnSubmit).toHaveBeenCalled();
-      });
-    });
-  });
   describe("SharedForm wrapping ProjectFormValues, happy paths", () => {
     const initialValues: ProjectFormValues = {
       name: "name",
