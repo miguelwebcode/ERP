95580a16f8db4ae00858bfdbdbda71a51662e3bb
miguelwebcode <miguelcodeonline@gmail.com>
2025-02-19 19:06:48 +0100

    Add test

--- client/src/views/customers/EditCustomerView/EditCustomerView.vitest.test.tsx
+++ client/src/views/customers/EditCustomerView/EditCustomerView.vitest.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { describe, it, expect, vi, Mock, beforeEach } from "vitest";
 import * as appStore from "../../../stores/app-store";
 import { useState, useRef } from "react";
@@ -122,5 +122,98 @@ describe("EditCustomerView", () => {
       name: /update customer/i,
     });
     expect(buttonUpdateCustomer).toBeInTheDocument();
+  });
+  /* 
+   TODO: Check with Guillem
+  */
+
+  it("fetch customer, check texts on form, update customer, check successful update", async () => {
+    const mockIsRenderDone = true;
+    const mockSetIsRenderDone = vi.fn();
+    (useState as Mock).mockReturnValue([mockIsRenderDone, mockSetIsRenderDone]);
+
+    const mockStore = {
+      selectedCustomerId: "260cf13e-84ec-4fd3-98e3-17d745a2a708",
+      setSelectedCustomerId: vi.fn(),
+    };
+    (appStore.useAppStore as unknown as Mock).mockImplementation((selector) =>
+      selector(mockStore)
+    );
+    const mockCustomerIds = [
+      "260cf13e-84ec-4fd3-98e3-17d745a2a708",
+      "31ccef4f-ab5b-4ddb-b031-6877b3e12891",
+    ];
+    const mockSetCustomerIds = vi.fn();
+    (useState as Mock).mockReturnValue([mockCustomerIds, mockSetCustomerIds]);
+
+    (useRef as Mock).mockImplementation(() => ({ current: false }));
+
+    render(<EditCustomerView />);
+
+    // SelectCustomerForm texts
+
+    const titleSelectCustomer = screen.getByRole("heading", {
+      name: /select customer/i,
+    });
+    expect(titleSelectCustomer).toBeInTheDocument();
+
+    const selectCustomerId = screen.getByLabelText(
+      /customer id/i
+    ) as HTMLSelectElement;
+    expect(selectCustomerId.value).toBe("");
+    expect(selectCustomerId.disabled).toBe(false);
+    fireEvent.change(selectCustomerId, {
+      target: { value: mockCustomerIds[0] },
+    });
+
+    const buttonSelectCustomer = screen.getByRole("button", {
+      name: /fetch customer/i,
+    }) as HTMLButtonElement;
+    expect(buttonSelectCustomer).toBeInTheDocument();
+    expect(buttonSelectCustomer.disabled).toBe(false);
+    fireEvent.click(buttonSelectCustomer);
+
+    await waitFor(() => {
+      // CustomerForm
+      const titleCustomerForm = screen.getByRole("heading", {
+        name: /Edit Customer/i,
+      });
+      expect(titleCustomerForm).toBeInTheDocument();
+
+      const inputAddress = screen.getByLabelText("Address") as HTMLInputElement;
+      expect(inputAddress).toBeInTheDocument();
+      expect(inputAddress.value).toBe("");
+      expect(inputAddress.disabled).toBe(true);
+
+      const inputCompany = screen.getByLabelText("Company") as HTMLInputElement;
+      expect(inputCompany).toBeInTheDocument();
+      expect(inputCompany.value).toBe("");
+      expect(inputCompany.disabled).toBe(true);
+
+      const inputEmail = screen.getByLabelText("Email") as HTMLInputElement;
+      expect(inputEmail).toBeInTheDocument();
+      expect(inputEmail.value).toBe("");
+      expect(inputEmail.disabled).toBe(true);
+
+      const inputName = screen.getByLabelText("Name") as HTMLInputElement;
+      expect(inputName).toBeInTheDocument();
+      expect(inputName.value).toBe("");
+      expect(inputName.disabled).toBe(true);
+
+      const inputPhone = screen.getByLabelText("Phone") as HTMLInputElement;
+      expect(inputPhone).toBeInTheDocument();
+      expect(inputPhone.value).toBe("");
+      expect(inputPhone.disabled).toBe(true);
+
+      const inputProject = screen.getByLabelText("Project") as HTMLInputElement;
+      expect(inputProject).toBeInTheDocument();
+      expect(inputProject.value).toBe("");
+      expect(inputProject.disabled).toBe(true);
+
+      const buttonUpdateCustomer = screen.getByRole("button", {
+        name: /update customer/i,
+      });
+      expect(buttonUpdateCustomer).toBeInTheDocument();
+    });
   });
 });
