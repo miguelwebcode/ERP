f80ec0e6b524aac4daa2d1e18354d7a58828b627
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-01 17:07:55 +0100

    Set functions return value

--- client/src/services/auth/repository/authRepository.ts
+++ client/src/services/auth/repository/authRepository.ts
@@ -15,7 +15,10 @@ export const firebaseLogin = async (email: string, password: string) => {
   }
 };
 
-export const firebaseRegisterUser = async (email: string, password: string) => {
+export const firebaseRegisterUser = async (
+  email: string,
+  password: string
+): Promise<UserCredential | undefined> => {
   try {
     return await createUserWithEmailAndPassword(auth, email, password);
   } catch (error) {
