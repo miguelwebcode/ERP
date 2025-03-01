5300b4888b4f7b2c86048f6b83ff142bfd428b1d
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-01 16:45:12 +0100

    Refactor code

--- client/src/services/auth/repository/authRepository.ts
+++ client/src/services/auth/repository/authRepository.ts
@@ -8,15 +8,27 @@ import {
 import { auth } from "../../../firebaseConfig";
 
 export const firebaseLogin = async (email: string, password: string) => {
-  return await signInWithEmailAndPassword(auth, email, password);
+  try {
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error("Login error: ", error);
+  }
 };
 
 export const firebaseRegisterUser = async (email: string, password: string) => {
-  return await createUserWithEmailAndPassword(auth, email, password);
+  try {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error("Register error: ", error);
+  }
 };
 
 export const firebaseLogout = async () => {
-  return await signOut(auth);
+  try {
+    return await signOut(auth);
+  } catch (error) {
+    console.error("Logout error: ", error);
+  }
 };
 
 export const firebaseOnAuthStateChanged = (
