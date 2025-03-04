5a87f0f09cbb862bc9ed0386627420f30de301df
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-04 19:31:51 +0100

    Create cypress config file

---
+++ client/cypress.config.ts
@@ -0,0 +1,24 @@
+import admin from "firebase-admin";
+import { defineConfig } from "cypress";
+import { plugin as cypressFirebasePlugin } from "cypress-firebase";
+
+export default defineConfig({
+  e2e: {
+    /* 
+     TODO: Ask guillem about port and config
+    */
+    baseUrl: "http://localhost:5173",
+    // NOTE: Add "supportFile" setting if separate location is used
+    setupNodeEvents(on, config) {
+      // e2e testing node events setup code
+      return cypressFirebasePlugin(on, config, admin, {
+        // Here is where you can pass special options.
+        // If you have not set the GCLOUD_PROJECT environment variable, give the projectId here, like so:
+        //    projectId: 'some-project',
+        projectId: "erp-fire",
+        // if your databaseURL is not just your projectId plus ".firebaseio.com", then you _must_ give it here, like so:
+        //    databaseURL: 'some-project-default-rtdb.europe-west1.firebasedatabase.app',
+      });
+    },
+  },
+});
