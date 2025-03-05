37030ccfa4360e96a5bc3c57f4735d4174a1c01e
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-05 17:28:01 +0100

    Import dotenv

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,6 +1,8 @@
 import admin from "firebase-admin";
 import { defineConfig } from "cypress";
 import { plugin as cypressFirebasePlugin } from "cypress-firebase";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   e2e: {
@@ -15,7 +17,6 @@ export default defineConfig({
         // Here is where you can pass special options.
         // If you have not set the GCLOUD_PROJECT environment variable, give the projectId here, like so:
         //    projectId: 'some-project',
-        projectId: "erp-fire",
         // if your databaseURL is not just your projectId plus ".firebaseio.com", then you _must_ give it here, like so:
         //    databaseURL: 'some-project-default-rtdb.europe-west1.firebasedatabase.app',
       });
