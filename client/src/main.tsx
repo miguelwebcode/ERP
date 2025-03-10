35dc8f7345af5fc92049f9fe831a1682c402d41a
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-10 16:38:59 +0100

    Import ReactToastify styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import AppRouter from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
