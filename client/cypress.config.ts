05a2a980889f3a548379318e0fdcefa8b60097d9
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-10 13:06:39 +0100

    Solve problem with cy.login

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -14,16 +14,31 @@ export default defineConfig({
       TEST_UID: process.env.CYPRESS_TEST_UID,
       CYPRESS_FIREBASE_API_KEY: process.env.CYPRESS_FIREBASE_API_KEY,
     },
+
     // NOTE: Add "supportFile" setting if separate location is used
     setupNodeEvents(on, config) {
       // e2e testing node events setup code
-      return cypressFirebasePlugin(on, config, admin, {
-        // Here is where you can pass special options.
-        // If you have not set the GCLOUD_PROJECT environment variable, give the projectId here, like so:
-        //    projectId: 'some-project',
-        // if your databaseURL is not just your projectId plus ".firebaseio.com", then you _must_ give it here, like so:
-        //    databaseURL: 'some-project-default-rtdb.europe-west1.firebasedatabase.app',
-      });
+      return cypressFirebasePlugin(
+        on,
+        config,
+        admin,
+        { projectId: "erp-fire" },
+        {
+          protectProduction: {
+            // when rtdb emulator isn't detected, a console warning will appear
+            rtdb: "warn",
+            // plugin will be indifferent to the firestore emulator running or not
+            firestore: "none",
+            // when auth emulator isn't detected, an error is thrown halting cypress
+            auth: "error",
+          },
+          // Here is where you can pass special options.
+          // If you have not set the GCLOUD_PROJECT environment variable, give the projectId here, like so:
+          //    projectId: 'some-project',
+          // if your databaseURL is not just your projectId plus ".firebaseio.com", then you _must_ give it here, like so:
+          //    databaseURL: 'some-project-default-rtdb.europe-west1.firebasedatabase.app',
+        }
+      );
     },
   },
 });
--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -7,14 +7,36 @@ import { attachCustomCommands } from "cypress-firebase";
 
 const fbConfig = {
   // Your config from Firebase Console
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDDZ_LRemHgQdbG3pJA7IOGUbABqOJaFgA",
+  authDomain: "erp-fire.firebaseapp.com",
+  projectId: "erp-fire",
+  storageBucket: "erp-fire.firebasestorage.app",
+  messagingSenderId: "440327598939",
+  appId: "1:440327598939:web:faba0f28a17db1f6fc607e",
 };
 
 firebase.initializeApp(fbConfig);
+
+const firestoreSettings = {
+  experimentalForceLongPolling: false,
+};
+// Pass long polling setting to Firestore when running in Cypress
+if (window.Cypress) {
+  // Needed for Firestore support in Cypress (see https://github.com/cypress-io/cypress/issues/6350)
+  firestoreSettings.experimentalForceLongPolling = true;
+}
+// powershell -Command "$env:SERVICE_ACCOUNT = Get-Content -Path .\sa_dev.json -Raw; npx cypress open""
+const shouldUseEmulator = window.location.hostname === "localhost";
+if (shouldUseEmulator) {
+  firebase.firestore().settings({
+    host: "localhost:8080",
+    ssl: false,
+  });
+}
+
+if (shouldUseEmulator) {
+  firebase.auth().useEmulator("http://127.0.0.1:9099");
+  console.debug("Using Auth emulator");
+}
 
 attachCustomCommands({ Cypress, cy, firebase });
