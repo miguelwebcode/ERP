acecffa9a5a1855d1e6e2c28bd9bf01c544be3e2
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-11 18:31:05 +0100

    Use logout icon

--- client/src/components/ui/LogoutButton/LogoutButton.tsx
+++ client/src/components/ui/LogoutButton/LogoutButton.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { logout } from "../../../services/auth/service/authService";
+import { CiLogout } from "react-icons/ci";
 
 export const LogoutButton = () => {
   const navigate = useNavigate();
@@ -10,10 +11,13 @@ export const LogoutButton = () => {
   };
   return (
     <button
-      className="bg-slate-600 hover:bg-slate-800 text-white py-2 px-4 mt-5 rounded"
+      className="bg-slate-800 hover:bg-slate-600 text-white  font-bold py-2 px-4 rounded"
       onClick={handleClick}
     >
-      Logout
+      <div className="flex gap-3 items-center">
+        <CiLogout className="text-xl" />
+        <p>Logout</p>
+      </div>
     </button>
   );
 };
