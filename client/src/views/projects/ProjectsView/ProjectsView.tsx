8d9c4fca48020093c8b7cf8be4f00050745bd0a0
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-11 20:16:23 +0100

    Change projects view texts

--- client/src/views/projects/ProjectsView/ProjectsView.tsx
+++ client/src/views/projects/ProjectsView/ProjectsView.tsx
@@ -5,30 +5,40 @@ export const ProjectsView = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col items-center justify-center bg-white p-6 rounded shadow-md gap-6">
-        <h1 className="uppercase font-bold text-xl">Project CRUD</h1>
-        <div className="flex flex-col gap-5">
+    <div className="flex flex-col items-center gap-10">
+      <h1 className="uppercase font-bold text-3xl">PROJECTS</h1>
+
+      <div className="flex flex-col gap-6 bg-white p-6 rounded shadow-md min-w-[300px]">
+        <h3 className="uppercase font-bold text-center text-xl">
+          MANAGE PROJECTS
+        </h3>
+        <div className="flex justify-around">
           <SharedButton
-            text="CREATE Project"
+            className="w-24 px-4"
+            text="CREATE"
             onClick={() => {
               navigate("/projects/add");
             }}
           />
           <SharedButton
-            text="READ Projects"
+            className="w-24"
+            text="READ"
             onClick={() => {
               navigate("/projects/read");
             }}
           />
+        </div>
+        <div className="flex justify-around">
           <SharedButton
-            text="UPDATE Project"
+            className="w-24 px-4"
+            text="UPDATE"
             onClick={() => {
               navigate("/projects/edit");
             }}
           />
           <SharedButton
-            text="DELETE Project"
+            className="w-24 px-4"
+            text="DELETE"
             onClick={() => {
               navigate("/projects/delete");
             }}
