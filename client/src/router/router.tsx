8316d6544cd982b58d729f9f1b35f9d2daeef543
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-15 18:23:09 +0100

    Move router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout";
-import { LoginView } from "./views/LoginView/LoginView";
-import { RegisterView } from "./views/RegisterView/RegisterView";
-import { HomeView } from "./views/HomeView/HomeView";
-import { CustomersView } from "./views/customers/CustomersView/CustomersView";
-import { ProjectsView } from "./views/projects/ProjectsView/ProjectsView";
-import { AddCustomerView } from "./views/customers/AddCustomerView/AddCustomerView";
-import { EditCustomerView } from "./views/customers/EditCustomerView/EditCustomerView";
-import { AddProjectView } from "./views/projects/AddProjectView/AddProjectView";
-import { EditProjectView } from "./views/projects/EditProjectView/EditProjectView";
-import { ReadCustomersView } from "./views/customers/ReadCustomersView/ReadCustomersView";
-import { DeleteCustomerView } from "./views/customers/DeleteCustomerView/DeleteCustomerView";
-import { ReadProjectsView } from "./views/projects/ReadProjectsView/ReadProjectsView";
-import { DeleteProjectView } from "./views/projects/DeleteProjectView/DeleteProjectView";
+import Layout from "../layouts/Layout";
+import { LoginView } from "../views/LoginView/LoginView";
+import { RegisterView } from "../views/RegisterView/RegisterView";
+import { HomeView } from "../views/HomeView/HomeView";
+import { CustomersView } from "../views/customers/CustomersView/CustomersView";
+import { ProjectsView } from "../views/projects/ProjectsView/ProjectsView";
+import { AddCustomerView } from "../views/customers/AddCustomerView/AddCustomerView";
+import { EditCustomerView } from "../views/customers/EditCustomerView/EditCustomerView";
+import { AddProjectView } from "../views/projects/AddProjectView/AddProjectView";
+import { EditProjectView } from "../views/projects/EditProjectView/EditProjectView";
+import { ReadCustomersView } from "../views/customers/ReadCustomersView/ReadCustomersView";
+import { DeleteCustomerView } from "../views/customers/DeleteCustomerView/DeleteCustomerView";
+import { ReadProjectsView } from "../views/projects/ReadProjectsView/ReadProjectsView";
+import { DeleteProjectView } from "../views/projects/DeleteProjectView/DeleteProjectView";
 
 export default function AppRouter() {
   return (
