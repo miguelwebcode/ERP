e084fe9f799245ed9a4f00d5af9ec0c87c130531
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-15 18:40:03 +0100

    Update router import route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
-import AppRouter from "./router.tsx";
+import AppRouter from "./router/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
