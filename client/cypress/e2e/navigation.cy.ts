2ee2e2dad48933952304f8859f9ff4e0b0d59a87
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-15 19:31:20 +0100

    Create navbar tests

--- client/cypress/e2e/navigation.cy.ts
+++ client/cypress/e2e/navigation.cy.ts
@@ -17,4 +17,31 @@ describe("Navigation bar", () => {
       cy.contains("Projects");
     });
   });
+  it("should navigate to customers section", () => {
+    const navBar = cy.get("nav");
+    navBar.should("be.visible");
+    navBar.within(() => {
+      const customersNavLink = cy.contains("Customers");
+      customersNavLink.click();
+    });
+    cy.url().should("match", /\/customers$/);
+  });
+  it("should navigate to projects section", () => {
+    const navBar = cy.get("nav");
+    navBar.should("exist");
+    navBar.within(() => {
+      cy.contains("Projects").click();
+    });
+    cy.url().should("match", /\/projects$/);
+  });
+  it("should navigate to home section", () => {
+    const navBar = cy.get("nav");
+    navBar.should("exist");
+    navBar.within(() => {
+      cy.contains("Customers").click();
+      cy.url().should("match", /\/customers$/);
+      cy.contains("Home").click();
+    });
+    cy.url().should("match", /\/$/);
+  });
 });
