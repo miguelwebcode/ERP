e3d363612f7cb35c34a85215364ccbc96dfa7001
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-20 18:03:01 +0100

    Create interface

--- client/cypress/support/types.d.ts
+++ client/cypress/support/types.d.ts
@@ -1,11 +1,24 @@
 export interface Customer {
-  customerId: string;
+  customerId?: string;
   name: string;
   email: string;
   address: string;
   company: string;
   phone: string;
   project: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface Project {
+  projectId?: string;
+  customerId: string;
+  description: string;
+  developer: string;
+  name: string;
+  state: string;
+  startDate: string;
+  endDate: string;
   createdAt?: string;
   updatedAt?: string;
 }
