1af043a766c4dd5146909be699268f18a7b99b8f
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-20 18:06:29 +0100

    Rename file

--- client/cypress/support/customers-utils.ts
+++ client/cypress/support/customers-utils.ts
@@ -84,24 +84,3 @@ export const addCustomer = async (customer: Customer): Promise<string> => {
     throw error;
   }
 };
-
-export const deleteProjectByField = async (
-  fieldName: string,
-  fieldValue: string
-) => {
-  const projectsRef = db.collection("projects");
-  const snapshot = await projectsRef.where(fieldName, "==", fieldValue).get();
-
-  if (snapshot.empty) {
-    console.log("No matching documents.");
-    return 0;
-  }
-
-  let deleteCount = 0;
-  for (const doc of snapshot.docs) {
-    await db.collection("projects").doc(doc.id).delete();
-    deleteCount++;
-    console.log("deleted doc with id =>", doc.id);
-  }
-  return deleteCount;
-};
