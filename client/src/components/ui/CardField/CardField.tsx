2692e0e033215574d04c8bafd73bff9f53d84f32
miguelwebcode <miguelcodeonline@gmail.com>
2025-03-23 22:42:35 +0100

    Add icon prop

--- client/src/components/ui/CardField/CardField.tsx
+++ client/src/components/ui/CardField/CardField.tsx
@@ -1,12 +1,22 @@
+import { IconType } from "react-icons/lib";
+
 type CardFieldProps = {
   label: string;
   value: string;
+  Icon?: IconType;
 };
 
-export const CardField = ({ label, value }: CardFieldProps) => {
+export const CardField = ({ label, value, Icon }: CardFieldProps) => {
   return (
     <div className="flex flex-col mb-4 gap-1 ">
-      <label className="block text-sm font-medium">{label}</label>
+      <div className="flex gap-2 items-center">
+        {Icon && (
+          <div>
+            <Icon className="text-lg" />
+          </div>
+        )}
+        <label className="block text-sm font-medium">{label}</label>
+      </div>
       <span data-testid={label} className="text-md">
         {value}
       </span>
