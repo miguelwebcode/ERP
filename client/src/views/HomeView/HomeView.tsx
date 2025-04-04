61bee288c4f697733ab9e7c922c767e35950b464
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-05 01:31:44 +0200

    Use Dashboard

--- client/src/views/HomeView/HomeView.tsx
+++ client/src/views/HomeView/HomeView.tsx
@@ -2,8 +2,7 @@ import { useNavigate } from "react-router-dom";
 
 import { useEffect } from "react";
 import { useAppStore } from "../../stores/app-store";
-import { CustomersNavigationCard } from "../../components/customers/CustomersNavigationCard/CustomersNavigationCard";
-import { ProjectsNavigationCard } from "../../components/projects/ProjectsNavigationCard/ProjectsNavigationCard";
+import Dashboard from "../../components/dashboard/Dashboard";
 
 export const HomeView = () => {
   const navigate = useNavigate();
@@ -15,14 +14,5 @@ export const HomeView = () => {
     }
   }, []);
 
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="uppercase font-bold text-3xl">HOME</h1>
-      <h2 className="my-8">Welcome, {user && user.email} </h2>
-      <div className="flex flex-col lg:flex-row gap-4 ">
-        <CustomersNavigationCard />
-        <ProjectsNavigationCard />
-      </div>
-    </div>
-  );
+  return <Dashboard />;
 };
