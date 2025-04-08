bdf23866cce6ca92d21c3b81b474f5759dfe9613
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-08 22:22:43 +0200

    Change styles

--- client/src/components/ui/Header/Header.tsx
+++ client/src/components/ui/Header/Header.tsx
@@ -3,6 +3,7 @@ import { NavLink, useLocation } from "react-router-dom";
 import { LiaGripfire } from "react-icons/lia";
 import { LogoutButton } from "../LogoutButton/LogoutButton";
 import { useAppStore } from "../../../stores/app-store";
+import "@styles/Header.css";
 
 export default function Header() {
   const user = useAppStore((state) => state.user);
@@ -23,21 +24,23 @@ export default function Header() {
     ];
     return user && paths.includes(pathname);
   }, [pathname]);
+
+  const navTextHighlight =
+    "text-ds-primary-400 text-ds-xl uppercase font-bold header-link-selected";
+  const navTextNormal = "text-ds-white text-ds-xl uppercase font-bold";
   return (
     <header className="bg-slate-800">
-      <div className="flex mx-auto container px-5 py-8 justify-between items-center">
-        <div className="flex gap-3 items-center text-white text-3xl">
+      <div className="flex mx-auto container px-ds-20 py-ds-32 justify-between items-center">
+        <div className="flex gap-3 items-center text-ds-white text-ds-2xl">
           <LiaGripfire />
           <h1>FirERP</h1>
         </div>
         {needsNavigation && (
-          <div className="inline-flex justify-center items-center gap-4">
-            <nav className="flex gap-4 text-xl">
+          <div className="inline-flex justify-center items-center gap-ds-16">
+            <nav className="flex gap-ds-32 text-ds-xl">
               <NavLink
                 className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-400 uppercase font-bold"
-                    : "text-white uppercase font-bold"
+                  isActive ? navTextHighlight : navTextNormal
                 }
                 to="/"
               >
@@ -45,9 +48,7 @@ export default function Header() {
               </NavLink>
               <NavLink
                 className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-400 uppercase font-bold"
-                    : "text-white uppercase font-bold"
+                  isActive ? navTextHighlight : navTextNormal
                 }
                 to="/customers"
               >
@@ -55,16 +56,14 @@ export default function Header() {
               </NavLink>
               <NavLink
                 className={({ isActive }) =>
-                  isActive
-                    ? "text-blue-400 uppercase font-bold"
-                    : "text-white uppercase font-bold"
+                  isActive ? navTextHighlight : navTextNormal
                 }
                 to="/projects"
               >
                 Projects
               </NavLink>
+              <LogoutButton />
             </nav>
-            <LogoutButton />
           </div>
         )}
       </div>
