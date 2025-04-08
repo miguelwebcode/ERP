5e733e0c35f12bf7f392ba9e42a232de66a1d5cf
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-08 20:17:47 +0200

    Create NavButton and modular css

---
+++ client/src/components/ui/NavButton/NavButton.tsx
@@ -0,0 +1,34 @@
+import { IconType } from "react-icons/lib";
+import "../../../styles/NavButton.css";
+
+type NavButtonProps = {
+  text: string;
+  onClick: () => void;
+  Icon?: IconType;
+};
+
+export const NavButton = ({
+  text,
+  onClick: handleClick,
+  Icon,
+}: NavButtonProps) => {
+  return (
+    <button
+      className={"nav-button"}
+      onClick={() => {
+        handleClick();
+      }}
+    >
+      <div className="flex justify-center items-center gap-ds-8">
+        {Icon && (
+          <span className="text-ds-primary-500">
+            <Icon className="text-ds-xl" />
+          </span>
+        )}
+        <p className="text-ds-lg font-semibold text-ds-primary-500 text-center">
+          {text}
+        </p>
+      </div>
+    </button>
+  );
+};
