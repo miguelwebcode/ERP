b2e15489b591d816dad1840eeee241d768859369
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-08 20:03:04 +0200

    Use NavButton

--- client/src/components/customers/CustomersNavigationCard/CustomersNavigationCard.tsx
+++ client/src/components/customers/CustomersNavigationCard/CustomersNavigationCard.tsx
@@ -1,53 +1,45 @@
 import { FaPlus } from "react-icons/fa";
 import { IoReader } from "react-icons/io5";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
-import { SharedButton } from "../../ui/SharedButton/SharedButton";
 import { useNavigate } from "react-router-dom";
+import { NavButton } from "../../ui/NavButton/NavButton";
 
 export const CustomersNavigationCard = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col gap-6 bg-white p-6 rounded shadow-md min-w-[300px]">
-      <h3 className="uppercase font-bold text-center text-xl">
+    <div className="flex flex-col gap-ds-24 bg-ds-white p-ds-32 rounded-ds-sm shadow-ds-2">
+      <h3 className="uppercase text-ds-lg font-bold text-center ">
         MANAGE CUSTOMERS
       </h3>
-      <div className="flex justify-around gap-6">
-        <SharedButton
-          className="flex-1 px-4"
-          text="CREATE"
-          Icon={FaPlus}
-          onClick={() => {
-            navigate("/customers/add");
-          }}
-        />
-        <SharedButton
-          className="flex-1 px-4"
-          text="READ"
-          Icon={IoReader}
-          onClick={() => {
-            navigate("/customers/read");
-          }}
-        />
-      </div>
-      <div className="flex justify-around gap-6">
-        <SharedButton
-          className="flex-1 px-4"
-          text="UPDATE"
-          Icon={MdEdit}
-          onClick={() => {
-            navigate("/customers/edit");
-          }}
-        />
-        <SharedButton
-          className="flex-1 px-4"
-          text="DELETE"
-          Icon={MdDeleteForever}
-          onClick={() => {
-            navigate("/customers/delete");
-          }}
-        />
-      </div>
+      <NavButton
+        text="CREATE"
+        Icon={FaPlus}
+        onClick={() => {
+          navigate("/customers/add");
+        }}
+      />
+      <NavButton
+        text="READ"
+        Icon={IoReader}
+        onClick={() => {
+          navigate("/customers/read");
+        }}
+      />
+      <NavButton
+        text="UPDATE"
+        Icon={MdEdit}
+        onClick={() => {
+          navigate("/customers/edit");
+        }}
+      />
+      <NavButton
+        text="DELETE"
+        Icon={MdDeleteForever}
+        onClick={() => {
+          navigate("/customers/delete");
+        }}
+      />
     </div>
   );
 };
