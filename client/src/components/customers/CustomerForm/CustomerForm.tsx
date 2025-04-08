e2edb27d6695a5b169b7831007cb36f6e6364ab1
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-08 19:35:33 +0200

    Remove projects from CustomerForm

--- client/src/components/customers/CustomerForm/CustomerForm.tsx
+++ client/src/components/customers/CustomerForm/CustomerForm.tsx
@@ -26,7 +26,6 @@ const CustomerForm = ({
   onSubmit: handleSubmit,
   canBeDisabled,
 }: CustomerFormProps) => {
-  const [projects, setProjects] = useState<Project[]>([]);
   const formikRef = useRef<FormikProps<CustomerFormValues>>(null);
   const initialValues: CustomerFormValues = {
     address: "",
@@ -34,13 +33,11 @@ const CustomerForm = ({
     email: "",
     name: "",
     phone: "",
-    project: "",
   };
 
   const selectedCustomerId = useAppStore((state) => state.selectedCustomerId);
 
   useEffect(() => {
-    fetchAllProjects(setProjects);
     /* 
      On first useEffect, form is not mounted yet, its reference formikRef.current
      is null. Then, on selectedCustomerId change, useEffect is executed again...
@@ -63,68 +60,51 @@ const CustomerForm = ({
       onSubmit={handleSubmit}
       innerRef={formikRef}
     >
-      <div className="flex flex-col items-center justify-center bg-white p-6 rounded shadow-md">
-        <h1 className="text-2xl font-bold mb-8">{titleText}</h1>
-        <div className="flex flex-col sm:flex-row sm:gap-10">
-          <div className="flex flex-col w-fit">
-            <CustomInput
-              type="text"
-              label="Name"
-              name="name"
-              placeholder="Enter name"
-              disabled={fieldDisabled}
-            />
-            <CustomInput
-              type="text"
-              label="Address"
-              name="address"
-              placeholder="Enter address"
-              disabled={fieldDisabled}
-            />
-            <CustomInput
-              type="text"
-              label="Email"
-              name="email"
-              placeholder="Enter email"
-              disabled={fieldDisabled}
-            />
-          </div>
-          <div className="flex flex-col w-fit">
-            <CustomInput
-              type="text"
-              label="Company"
-              name="company"
-              placeholder="Enter company"
-              disabled={fieldDisabled}
-            />
-            <CustomInput
-              type="text"
-              label="Phone"
-              name="phone"
-              placeholder="Enter phone"
-              disabled={fieldDisabled}
-            />
-            <CustomSelect
-              label="Project"
-              name="project"
-              disabled={fieldDisabled}
-            >
-              <option value="">-- Select an option --</option>
-              <option value="none">None</option>
-              {projects.map((project, index) => (
-                <option key={index} value={project.name}>
-                  {project.name}
-                </option>
-              ))}
-            </CustomSelect>
-          </div>
+      <div className="flex flex-col items-center justify-center bg-ds-white p-ds-32 rounded-ds-sm shadow-ds-2">
+        <h1 className="text-ds-xl font-bold mb-ds-32">{titleText}</h1>
+        <div className="grid grid-cols-2 gap-y-ds-20 gap-x-ds-20">
+          <CustomInput
+            type="text"
+            label="Name"
+            name="name"
+            placeholder="Enter name"
+            disabled={fieldDisabled}
+          />
+          <CustomInput
+            type="text"
+            label="Company"
+            name="company"
+            placeholder="Enter company"
+            disabled={fieldDisabled}
+          />
+          <CustomInput
+            type="text"
+            label="Address"
+            name="address"
+            placeholder="Enter address"
+            disabled={fieldDisabled}
+          />
+          <CustomInput
+            type="text"
+            label="Phone"
+            name="phone"
+            placeholder="Enter phone"
+            disabled={fieldDisabled}
+          />
+          <CustomInput
+            type="text"
+            label="Email"
+            name="email"
+            placeholder="Enter email"
+            disabled={fieldDisabled}
+          />
         </div>
         <button
           type="submit"
-          className="w-fit bg-blue-500 text-white mt-4 py-2 px-4 rounded hover:bg-blue-600 uppercase font-bold disabled:bg-gray-500"
+          className="w-ds-192 bg-ds-primary-500 text-ds-white mt-ds-32 py-ds-12 px-ds-16 rounded-ds-lg hover:bg-ds-primary-600 uppercase font-bold disabled:bg-ds-grey-400"
           disabled={fieldDisabled}
         >
-          {submitButtonText}
+          <p className="text-ds-lg">{submitButtonText}</p>
         </button>
       </div>
     </SharedForm>
