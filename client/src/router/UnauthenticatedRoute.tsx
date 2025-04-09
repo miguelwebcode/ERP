e91a53926bb371179a38c9d590e5d1e2434376e4
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-09 11:55:06 +0200

    Modify logic, show spinner

--- client/src/router/UnauthenticatedRoute.tsx
+++ client/src/router/UnauthenticatedRoute.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppStore } from "../stores/app-store";
 import { watchAuthState } from "../services/auth/service/authService";
 import { Navigate, Outlet } from "react-router-dom";
+import Spinner from "../components/ui/Spinner/Spinner";
 
 type UnauthenticatedRouteProps = {
   redirectRoute?: string;
@@ -12,10 +13,27 @@ export const UnauthenticatedRoute = ({
 }: UnauthenticatedRouteProps) => {
   const user = useAppStore((state) => state.user);
   const setUser = useAppStore((state) => state.setUser);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    watchAuthState(setUser);
-  }, [user]);
+    const unsubscribe = watchAuthState((authUser) => {
+      setUser(authUser);
+      setIsLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col justify-center items-center gap-ds-16 h-[70vh]">
+        <Spinner />
+        <p className="text-ds-base">Loading...</p>
+      </div>
+    );
+  }
 
   return user ? <Navigate to={redirectRoute} replace /> : <Outlet />;
 };
