82cf1e8244d8107e1cc1783482068bfb258cf0dc
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-09 10:53:22 +0200

    Remove user check

--- client/src/views/HomeView/HomeView.tsx
+++ client/src/views/HomeView/HomeView.tsx
@@ -1,18 +1,5 @@
-import { useNavigate } from "react-router-dom";
-
-import { useEffect } from "react";
-import { useAppStore } from "../../stores/app-store";
-import Dashboard from "../../components/dashboard/Dashboard";
+import Dashboard from "../../components/Dashboard/Dashboard";
 
 export const HomeView = () => {
-  const navigate = useNavigate();
-  const user = useAppStore((state) => state.user);
-
-  useEffect(() => {
-    if (user === null) {
-      navigate("/login");
-    }
-  }, []);
-
   return <Dashboard />;
 };
