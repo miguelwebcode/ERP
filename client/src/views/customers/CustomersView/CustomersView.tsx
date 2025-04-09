d734f48da2493889b5175f536b7af7303794a972
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-09 12:32:56 +0200

    Modify layout

--- client/src/views/customers/CustomersView/CustomersView.tsx
+++ client/src/views/customers/CustomersView/CustomersView.tsx
@@ -2,9 +2,14 @@ import { CustomersNavigationCard } from "../../../components/customers/Customers
 
 export const CustomersView = () => {
   return (
-    <div className="flex flex-col items-center gap-10">
-      <h1 className="uppercase font-bold text-3xl">CUSTOMERS</h1>
+    <div className="flex items-center h-[70vh]">
       <CustomersNavigationCard />
+      <div className="flex flex-1  items-center justify-center">
+        <p className="font-bold text-ds-lg text-ds-grey-600 text-center">
+          Select an option to{" "}
+          <span className="text-ds-primary-500">manage customers</span>
+        </p>
+      </div>
     </div>
   );
 };
