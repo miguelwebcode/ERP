7d4893ef76bda4fdefa1ab5acb49e172df8a2111
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-09 14:26:24 +0200

    Add prop activateOnRoute

--- client/src/components/ui/NavButton/NavButton.tsx
+++ client/src/components/ui/NavButton/NavButton.tsx
@@ -1,20 +1,29 @@
 import "@styles/NavButton.css";
 import { IconType } from "react-icons/lib";
+import { useLocation } from "react-router-dom";
 
 type NavButtonProps = {
   text: string;
   onClick: () => void;
+  activatedOnRoute: string;
   Icon?: IconType;
 };
 
 export const NavButton = ({
   text,
   onClick: handleClick,
+  activatedOnRoute,
   Icon,
 }: NavButtonProps) => {
+  const { pathname: currentRoute } = useLocation();
+
   return (
     <button
-      className={"nav-button"}
+      className={
+        currentRoute.includes(activatedOnRoute)
+          ? "activated-on-route"
+          : "nav-button"
+      }
       onClick={() => {
         handleClick();
       }}
