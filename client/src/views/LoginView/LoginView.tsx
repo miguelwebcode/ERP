41ae0b15a8aac561395a2438f8c04db9db907a44
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-09 11:13:21 +0200

    Remove unnecesary redirect logic

--- client/src/views/LoginView/LoginView.tsx
+++ client/src/views/LoginView/LoginView.tsx
@@ -1,19 +1,5 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import LoginForm from "../../components/login/LoginForm/LoginForm";
-import { watchAuthState } from "../../services/auth/service/authService";
-import { useAppStore } from "../../stores/app-store";
 
 export const LoginView = () => {
-  const navigate = useNavigate();
-  const user = useAppStore((state) => state.user);
-  const setUser = useAppStore((state) => state.setUser);
-
-  useEffect(() => {
-    watchAuthState(setUser);
-    if (user != null) {
-      navigate("/");
-    }
-  }, [user]);
   return <LoginForm />;
 };
