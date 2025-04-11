55e6f984b57891968bc899571eb4a1c57c2b159f
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-11 21:50:00 +0200

    Refactor NavButton

--- client/src/components/ui/NavButton/NavButton.tsx
+++ client/src/components/ui/NavButton/NavButton.tsx
@@ -1,31 +1,24 @@
 import "@styles/NavButton.css";
 import { IconType } from "react-icons/lib";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 type NavButtonProps = {
   text: string;
-  onClick: () => void;
-  activatedOnRoute: string;
+  route: string;
   Icon?: IconType;
 };
 
-export const NavButton = ({
-  text,
-  onClick: handleClick,
-  activatedOnRoute,
-  Icon,
-}: NavButtonProps) => {
+export const NavButton = ({ text, route, Icon }: NavButtonProps) => {
   const { pathname: currentRoute } = useLocation();
+  const navigate = useNavigate();
 
   return (
     <button
       className={
-        currentRoute.includes(activatedOnRoute)
-          ? "activated-on-route"
-          : "nav-button"
+        currentRoute.includes(route) ? "activated-on-route" : "nav-button"
       }
       onClick={() => {
-        handleClick();
+        navigate(route);
       }}
     >
       <div className="flex justify-center items-center gap-ds-8">
