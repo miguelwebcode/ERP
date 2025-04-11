9399b93abb9f019b1cd1a84345c3dc31040cfe50
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-11 21:49:35 +0200

    Refactor NavButtons

--- client/src/components/customers/CustomersNavigationCard/CustomersNavigationCard.tsx
+++ client/src/components/customers/CustomersNavigationCard/CustomersNavigationCard.tsx
@@ -1,48 +1,21 @@
 import { FaPlus } from "react-icons/fa";
 import { IoReader } from "react-icons/io5";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
 import { NavButton } from "../../ui/NavButton/NavButton";
 
 export const CustomersNavigationCard = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="flex flex-col gap-ds-24 bg-ds-white p-ds-32 rounded-ds-sm shadow-ds-2 w-ds-256">
       <h3 className="uppercase text-ds-lg font-bold text-center ">
         MANAGE CUSTOMERS
       </h3>
-      <NavButton
-        text="CREATE"
-        Icon={FaPlus}
-        activatedOnRoute="customers/add"
-        onClick={() => {
-          navigate("/customers/add");
-        }}
-      />
-      <NavButton
-        text="READ"
-        Icon={IoReader}
-        activatedOnRoute="customers/read"
-        onClick={() => {
-          navigate("/customers/read");
-        }}
-      />
-      <NavButton
-        text="UPDATE"
-        Icon={MdEdit}
-        activatedOnRoute="/customers/edit"
-        onClick={() => {
-          navigate("/customers/edit");
-        }}
-      />
+      <NavButton text="CREATE" Icon={FaPlus} route="/customers/add" />
+      <NavButton text="READ" Icon={IoReader} route="/customers/read" />
+      <NavButton text="UPDATE" Icon={MdEdit} route="/customers/edit" />
       <NavButton
         text="DELETE"
         Icon={MdDeleteForever}
-        activatedOnRoute="/customers/delete"
-        onClick={() => {
-          navigate("/customers/delete");
-        }}
+        route="/customers/delete"
       />
     </div>
   );
--- client/src/components/projects/ProjectsNavigationCard/ProjectsNavigationCard.tsx
+++ client/src/components/projects/ProjectsNavigationCard/ProjectsNavigationCard.tsx
@@ -1,48 +1,21 @@
 import { FaPlus } from "react-icons/fa";
 import { IoReader } from "react-icons/io5";
 import { MdEdit, MdDeleteForever } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
 import { NavButton } from "../../ui/NavButton/NavButton";
 
 export const ProjectsNavigationCard = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="flex flex-col gap-ds-24 bg-ds-white p-ds-32 rounded-ds-sm shadow-ds-2 w-ds-256">
       <h3 className="uppercase text-ds-lg font-bold text-center ">
         MANAGE PROJECTS
       </h3>
-      <NavButton
-        text="CREATE"
-        Icon={FaPlus}
-        activatedOnRoute="/projects/add"
-        onClick={() => {
-          navigate("/projects/add");
-        }}
-      />
-      <NavButton
-        text="READ"
-        Icon={IoReader}
-        activatedOnRoute="/projects/read"
-        onClick={() => {
-          navigate("/projects/read");
-        }}
-      />
-      <NavButton
-        text="UPDATE"
-        Icon={MdEdit}
-        activatedOnRoute="/projects/edit"
-        onClick={() => {
-          navigate("/projects/edit");
-        }}
-      />
+      <NavButton text="CREATE" Icon={FaPlus} route="/projects/add" />
+      <NavButton text="READ" Icon={IoReader} route="/projects/read" />
+      <NavButton text="UPDATE" Icon={MdEdit} route="/projects/edit" />
       <NavButton
         text="DELETE"
         Icon={MdDeleteForever}
-        activatedOnRoute="/projects/delete"
-        onClick={() => {
-          navigate("/projects/delete");
-        }}
+        route="/projects/delete"
       />
     </div>
   );
