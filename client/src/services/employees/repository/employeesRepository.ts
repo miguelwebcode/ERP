c8791a3614eb8ae499de1125a7392d2ab7b8d2e5
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-11 14:32:01 +0200

    Create employee repository functions

---
+++ client/src/services/employees/repository/employeesRepository.ts
@@ -0,0 +1,132 @@
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../../../firebaseConfig";
+import { v4 as uuidv4 } from "uuid";
+import { FormikHelpers } from "formik";
+import { EmployeeFormValues } from "../../../types/form-values-types";
+import { formatDate } from "../..";
+import { toast } from "react-toastify";
+
+export const getAllEmployees = async () => {
+  const employeesCollection = collection(db, "employees");
+  try {
+    const querySnapshot = await getDocs(employeesCollection);
+
+    const employees = querySnapshot.docs.map((doc) => doc.data());
+    return employees;
+  } catch (error) {
+    console.error("Error reading employees: ", error);
+  }
+};
+
+export const getEmployeeById = async (employeeId: string) => {
+  if (!employeeId) {
+    console.log("Employee ID is empty");
+    return;
+  }
+
+  const employeesCollection = collection(db, "employees");
+  const q = query(employeesCollection, where("id", "==", employeeId));
+
+  try {
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      console.log("No matching documents.");
+      return null;
+    }
+
+    const employeeData = querySnapshot.docs[0].data();
+    console.log("Employee data: ", employeeData);
+    return employeeData;
+  } catch (error) {
+    console.error("Error reading employee: ", error);
+  }
+};
+
+export const getAllEmployeeIds = async () => {
+  const employeesCollection = collection(db, "employees");
+  try {
+    const querySnapshot = await getDocs(employeesCollection);
+    const employeeIds = querySnapshot.docs.map((doc) => doc.data().id);
+    console.log("Employee IDs: ", employeeIds);
+    return employeeIds;
+  } catch (error) {
+    console.error("Error reading employee IDs: ", error);
+  }
+};
+
+export const handleCreateEmployee = async (
+  values: EmployeeFormValues,
+  formikHelpers: FormikHelpers<EmployeeFormValues>
+) => {
+  try {
+    await addDoc(collection(db, "employees"), {
+      ...values,
+      createdAt: formatDate(new Date()),
+      id: `E-${uuidv4().slice(0, 8)}`,
+    });
+    toast.success("Employee created");
+    formikHelpers.resetForm();
+  } catch (error) {
+    console.error("Error creating employee: ", error);
+  }
+};
+
+export const handleEditEmployee = async (
+  selectedEmployeeId: string,
+  values: EmployeeFormValues,
+  formikHelpers: FormikHelpers<EmployeeFormValues>
+) => {
+  try {
+    const employeesCollection = collection(db, "employees");
+    const q = query(employeesCollection, where("id", "==", selectedEmployeeId));
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      console.log("No matching documents.");
+      return null;
+    }
+
+    const documentId = querySnapshot.docs[0].id;
+
+    const employeeDocRef = doc(db, "employees", documentId);
+
+    await updateDoc(employeeDocRef, {
+      ...values,
+      updatedAt: formatDate(new Date()),
+    });
+    toast.success("Employee updated");
+    formikHelpers.resetForm();
+  } catch (error) {
+    console.error("Error updating employee: ", error);
+  }
+};
+
+export const deleteEmployeeById = async (employeeId: string) => {
+  const employeesCollection = collection(db, "employees");
+  const q = query(employeesCollection, where("id", "==", employeeId));
+
+  try {
+    const querySnapshot = await getDocs(q);
+    if (querySnapshot.empty) {
+      console.log("No matching documents.");
+      return null;
+    }
+
+    const documentId = querySnapshot.docs[0].id;
+    const employeeDocRef = doc(db, "employees", documentId);
+
+    await deleteDoc(employeeDocRef);
+    toast.success("Employee deleted");
+  } catch (error) {
+    console.error("Error deleting employee: ", error);
+  }
+};
