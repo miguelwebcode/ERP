a7bbc18932f166e0b0df138a6a0e9e77a9e52e5b
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-11 15:46:33 +0200

    Create employees view

---
+++ client/src/views/employees/EmployeesView/EmployeesView.tsx
@@ -0,0 +1,8 @@
+export const EmployeesView = () => {
+  return (
+    <p className="font-bold text-ds-lg text-ds-grey-600 text-center">
+      Select an option to{" "}
+      <span className="text-ds-primary-500">manage employee</span>
+    </p>
+  );
+};
