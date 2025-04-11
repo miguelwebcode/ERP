79a9026735e43ca5fe47f10b9be5839940690f83
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-11 15:55:03 +0200

    Modify text

--- client/src/views/employees/EmployeesView/EmployeesView.tsx
+++ client/src/views/employees/EmployeesView/EmployeesView.tsx
@@ -2,7 +2,7 @@ export const EmployeesView = () => {
   return (
     <p className="font-bold text-ds-lg text-ds-grey-600 text-center">
       Select an option to{" "}
-      <span className="text-ds-primary-500">manage employee</span>
+      <span className="text-ds-primary-500">manage employees</span>
     </p>
   );
 };
