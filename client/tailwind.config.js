6740c0fab0a63be953cc8599c9fe6862a61fd404
miguelwebcode <miguelcodeonline@gmail.com>
2025-04-13 20:06:32 +0200

    Configure shadcn

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
@@ -73,8 +76,47 @@ export default {
           disabled: "#F2F2F2",
         },
         "ds-black": "#1A1A1A",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
       },
-
       spacing: {
         "ds-4": "4px",
         "ds-8": "8px",
@@ -94,7 +136,6 @@ export default {
         "ds-640": "640px",
         "ds-768": "768px",
       },
-
       fontSize: {
         "ds-xxs": "10px",
         "ds-xs": "12px",
@@ -124,7 +165,6 @@ export default {
         "ds-normal": "0em",
         "ds-wide": "0.05em",
       },
-
       borderWidth: {
         "ds-thin": "1px",
         "ds-regular": "2px",
@@ -136,8 +176,10 @@ export default {
         "ds-lg": "12px",
         "ds-xl": "16px",
         "ds-full": "9999px",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-
       boxShadow: {
         "ds-1": "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
         "ds-2": "0 3px 6px rgba(0,0,0,0.15), 0 2px 4px rgba(0,0,0,0.12)",
@@ -147,5 +189,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 };
