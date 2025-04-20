e83e9f7bd6cab968fe3893e14d768906a35f36b3
mDev <miguelcodeonline@gmail.com>
2025-04-20 23:42:35 +0200

    Show trigger on mobile

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -27,7 +27,7 @@ export default function Layout() {
       <SidebarProvider open={sidebarOpen} onOpenChange={toggleSidebar}>
         {/* <Header /> */}
         <AppSidebar/>
-        <SidebarTrigger/>
+        <div className="block md:hidden"><SidebarTrigger/></div>
 
         <main className="container mx-auto pt-8">
           <Outlet />
