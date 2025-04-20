23f4e538c81c6fcc73d41b00fb3b0e0efed7b955
mDev <miguelcodeonline@gmail.com>
2025-04-20 23:34:14 +0200

    Persist sidebar open state

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,8 +1,27 @@
+import Cookies from 'js-cookie'
+
 import { Outlet } from "react-router-dom";
-import Header from "../components/ui/Header/Header";
 import { ToastContainer } from "react-toastify";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
+import { useEffect, useState } from 'react';
 
 export default function Layout() {
+  const [sidebarOpen, setSidebarOpen] = useState(() => {
+    const cookieValue = Cookies.get("sidebar_state");
+    return cookieValue === "true";
+  });
+
+  useEffect(() => {
+    Cookies.set("sidebar_state", String(sidebarOpen)); // Guarda el estado actual en la cookie
+  }, [sidebarOpen]);
+
+  const toggleSidebar = (isOpen:boolean) => {
+    setSidebarOpen(isOpen);
+  };
+
+  
+
   return (
     <>
       <SidebarProvider open={sidebarOpen} onOpenChange={toggleSidebar}>
@@ -10,10 +29,10 @@ export default function Layout() {
         <AppSidebar/>
         <SidebarTrigger/>
 
-      <main className="container mx-auto pt-8">
-        <Outlet />
-      </main>
-      <ToastContainer position="top-right" autoClose={3000} />
+        <main className="container mx-auto pt-8">
+          <Outlet />
+        </main>
+        <ToastContainer position="top-right" autoClose={3000} />
       </SidebarProvider>
     </>
   );
