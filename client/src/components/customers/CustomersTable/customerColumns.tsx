0bc542af4a694c8c7d81a345ccf19dca0c47b579
mDev <miguelcodeonline@gmail.com>
2025-04-23 22:08:29 +0200

    Set customer id and navigate edit customers

--- client/src/components/customers/CustomersTable/customerColumns.tsx
+++ client/src/components/customers/CustomersTable/customerColumns.tsx
@@ -19,31 +19,36 @@ const CustomerActions = ({ customer }: { customer: Customer }) => {
   );
   const navigate = useNavigate();
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(customer.id)}
-            >
-              <Copy className="mr-2 h-4 w-4" /> Copy customer ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <Edit className="mr-2 h-4 w-4" /> Edit customer
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Trash className="mr-2 h-4 w-4" /> Delete customer
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => navigator.clipboard.writeText(customer.id)}
+        >
+          <Copy className="mr-2 h-4 w-4" /> Copy customer ID
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            setSelectedCustomerId(customer.id);
+            navigate("/customers/edit");
+          }}
+        >
+          <Edit className="mr-2 h-4 w-4" /> Edit customer
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className="mr-2 h-4 w-4" /> Delete customer
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export const customerColumns: ColumnDef<Customer>[] = [
