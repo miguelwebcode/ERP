92f0f0d57aa153056db90b1eb6e268ec2652d0e7
mDev <miguelcodeonline@gmail.com>
2025-04-23 18:17:49 +0200

    Hide sidebar on auth routes

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,12 +1,19 @@
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/ui/app-sidebar";
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from "react";
 
 export default function Layout() {
+  const { pathname } = useLocation();
+  const hideSidebar = useMemo(() => {
+    const paths = ["/login", "/register"];
+
+    return paths.includes(pathname);
+  }, [pathname]);
+
   const [sidebarOpen, setSidebarOpen] = useState(() => {
     const cookieValue = Cookies.get("sidebar_state");
     return cookieValue === "true";
@@ -16,20 +23,19 @@ export default function Layout() {
     Cookies.set("sidebar_state", String(sidebarOpen)); // Guarda el estado actual en la cookie
   }, [sidebarOpen]);
 
-  const toggleSidebar = (isOpen:boolean) => {
+  const toggleSidebar = (isOpen: boolean) => {
     setSidebarOpen(isOpen);
   };
-
-  
 
   return (
     <>
       <SidebarProvider open={sidebarOpen} onOpenChange={toggleSidebar}>
-        {/* <Header /> */}
-        <AppSidebar/>
-        <SidebarTrigger className='block md:hidden'/>
+        <div className={`${hideSidebar && "hidden"}`}>
+          <AppSidebar />
+          <SidebarTrigger className="block md:hidden" />
+        </div>
 
-        <main className="container mx-auto md:mx-4 pt-8">
+        <main className="w-full mx-auto md:mx-4 pt-8">
           <Outlet />
         </main>
         <ToastContainer position="top-right" autoClose={3000} />
