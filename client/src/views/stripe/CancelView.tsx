bcf7841a5aa7091f93c36a9cabe8ec0f7f2e80f7
mDev <miguelcodeonline@gmail.com>
2025-04-27 00:45:08 +0200

    Create cancel view

---
+++ client/src/views/stripe/CancelView.tsx
@@ -0,0 +1,8 @@
+export const CancelView = () => {
+  return (
+    <div>
+      <h1>Pago cancelado</h1>
+      <p>Tu compra no se ha procesado. Puedes intentar de nuevo más tarde.</p>
+    </div>
+  );
+};
