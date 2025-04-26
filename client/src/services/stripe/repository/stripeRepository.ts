95fb2b33cf86fe822a61e5bb877ce5b2a1daa88d
mDev <miguelcodeonline@gmail.com>
2025-04-27 00:40:17 +0200

    Create stripe checkout session details callable function

--- client/src/services/stripe/repository/stripeRepository.ts
+++ client/src/services/stripe/repository/stripeRepository.ts
@@ -1,8 +1,14 @@
 import { functions } from "@/firebaseConfig";
 import { CheckoutSessionData, CheckoutSessionResponse } from "@/types/stripe";
 import { httpsCallable } from "firebase/functions";
+import type Stripe from "stripe";
 
 export const createCheckoutSession = httpsCallable<
   CheckoutSessionData,
   CheckoutSessionResponse
 >(functions, "createCheckoutSession");
+
+export const getCheckoutSessionDetails = httpsCallable<
+  { sessionId: string },
+  Stripe.Checkout.Session
+>(functions, "getCheckoutSessionDetails");
