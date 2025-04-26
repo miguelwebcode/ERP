7d5dbd70c186cadb02624bdab5d1d9de06892981
mDev <miguelcodeonline@gmail.com>
2025-04-26 23:51:35 +0200

    Rename file

--- client/src/lib/firebase/callCloudFunction.ts
+++ client/src/lib/firebase/callCloudFunction.ts
@@ -1,6 +1,6 @@
 import { httpsCallable } from "firebase/functions";
 import { functions } from "@/firebaseConfig";
 
-export const callCloud = (name: string) => {
+export const callCloudFunction = (name: string) => {
   return httpsCallable(functions, name);
 };
