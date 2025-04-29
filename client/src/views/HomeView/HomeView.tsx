90e05e8e7153c50427877bc1f6c406729d3d2067
mDev <miguelcodeonline@gmail.com>
2025-04-29 22:11:12 +0200

    Remove subscription button

--- client/src/views/HomeView/HomeView.tsx
+++ client/src/views/HomeView/HomeView.tsx
@@ -1,10 +1,8 @@
-import { SubscriptionButton } from "@/components/ui/SubscriptionButton";
 import Dashboard from "../../components/dashboard/Dashboard";
 
 export const HomeView = () => {
   return (
     <>
-      <SubscriptionButton />
       <Dashboard />
     </>
   );
