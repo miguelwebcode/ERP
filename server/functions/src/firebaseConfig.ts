e8f8dde11b74c7d842391f22b1e99fd005977b09
mDev <miguelcodeonline@gmail.com>
2025-04-29 22:19:20 +0200

    Add webhook secret to functions config file

--- server/functions/src/firebaseConfig.ts
+++ server/functions/src/firebaseConfig.ts
@@ -10,11 +10,10 @@ let app: any;
 
 dotenv.config();
 
-dotenv.config();
-
 app = initializeApp();
 
 export const stripeSecret = process.env.STRIPE_SECRET as string;
+export const webhookSecret = process.env.WEBHOOK_SECRET as string;
 export const stripe = new Stripe(stripeSecret, {
   apiVersion: "2025-03-31.basil",
 });
