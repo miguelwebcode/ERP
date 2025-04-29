25041be1faa17ae3be09fc778ab3f08e69b19b20
mDev <miguelcodeonline@gmail.com>
2025-04-29 22:30:30 +0200

    Modify stripe products view

--- client/src/views/stripe/StripeProductsView.tsx
+++ client/src/views/stripe/StripeProductsView.tsx
@@ -1,28 +1,11 @@
-import { getCurrencySymbol } from "@/lib/utils";
-import {
-  fetchStripeProducts,
-  startProductCheckout,
-} from "@/services/stripe/service/stripeService";
+import { StripeProductCard } from "@/components/stripe/StripeProductCard";
+import { fetchStripeProducts } from "@/services/stripe/service/stripeService";
 import { StripeProduct } from "@/types/stripe-types";
 import { useEffect, useState } from "react";
 
 export const StripeProductsView = () => {
   const [products, setProducts] = useState<StripeProduct[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const projectId = "ThRZCXIv8nsnhqbE3QSx";
-
-  const handleCheckout = async (
-    priceId: string,
-    projectId: string,
-    mode: "recurring" | "one_time"
-  ) => {
-    try {
-      const modeValue = mode === "recurring" ? "subscription" : "payment";
-      await startProductCheckout(priceId, projectId, modeValue);
-    } catch (error) {
-      console.error("Error al crear sesión: ", error);
-    }
-  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -43,34 +26,10 @@ export const StripeProductsView = () => {
   }
   return (
     <>
-      <div className="flex flex-wrap gap-3">
+      <div className="flex flex-wrap justify-center gap-3">
         {products.map((product) => {
           const firstPrice = product.prices[0];
-          return (
-            <div className="border flex flex-col w-2/4 rounded-lg p-4 shadow-md bg-white mb-4">
-              <p className="text-lg font-bold mb-2">{product.name}</p>
-              <p className="text-green-600 text-base mb-2">{`${
-                firstPrice.amount / 100
-              }${getCurrencySymbol(firstPrice.currency)}`}</p>
-              {firstPrice.type === "recurring" ? (
-                <p className="text-sm text-gray-600">{`every ${firstPrice.recurring?.interval_count} ${firstPrice.recurring?.interval}s`}</p>
-              ) : (
-                <p className="text-sm text-gray-600">unique payment</p>
-              )}
-              <button
-                className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
-                onClick={() => {
-                  handleCheckout(
-                    firstPrice.priceId,
-                    projectId,
-                    firstPrice.type
-                  );
-                }}
-              >
-                PAY
-              </button>
-            </div>
-          );
+          return <StripeProductCard name={product.name} price={firstPrice} />;
         })}
       </div>
     </>
