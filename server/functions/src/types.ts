b45e1258c55690bdc9e0694b8defebdb306a4d68
mDev <miguelcodeonline@gmail.com>
2025-04-29 22:13:14 +0200

    Add cloud function types

--- server/functions/src/types.ts
+++ server/functions/src/types.ts
@@ -1,5 +1,3 @@
-import Stripe from "stripe";
-
 export type CheckoutSessionData = {
   priceId: string;
   projectId: string;
@@ -13,4 +11,26 @@ export type CheckoutSessionResponse = {
 
 export type CheckoutSessionDetailsData = {
   sessionId: string;
+};
+
+export type StripePriceDetails = {
+  priceId: string;
+  amount: number;
+  currency: string;
+  recurring: {
+    interval: "day" | "week" | "month" | "year";
+    interval_count: number;
+  } | null;
+  type: "one_time" | "recurring";
+};
+
+export type StripeProduct = {
+  productId: string;
+  name: string;
+  description: string | null;
+  prices: StripePriceDetails[];
+};
+
+export type ListStripeProductsResponse = {
+  products: StripeProduct[];
 };
