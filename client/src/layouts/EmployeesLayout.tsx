ca6c8b48b2f97eca755afed6216ae020ade13cce
mDev <miguelcodeonline@gmail.com>
2025-04-30 23:58:05 +0200

    Responsive emplyee layout

--- client/src/layouts/EmployeesLayout.tsx
+++ client/src/layouts/EmployeesLayout.tsx
@@ -3,7 +3,7 @@ import { EmployeesNavigationCard } from "../components/employees/EmployeesNaviga
 
 export default function EmployeesLayout() {
   return (
-    <div className="flex items-start h-[80vh] pt-4">
+    <div className="flex flex-col md:flex-row items-center md:items-start h-[80vh] pt-4 gap-4 ">
       <EmployeesNavigationCard />
       <div className="flex flex-1 items-center justify-center">
         <Outlet />
