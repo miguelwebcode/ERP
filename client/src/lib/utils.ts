f7e2dd829471f4738f51c0114db203ef4aa06af3
mDev <miguelcodeonline@gmail.com>
2025-04-30 22:26:54 +0200

    Create function to format amount and curency

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,20 +5,14 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getCurrencySymbol(
-  currencyCode: string,
-  locale = "en-US"
+export function formatCurrency(
+  amount: number,
+  currency: string,
+  locale: string = navigator.language
 ): string {
-  // Formatea el valor 0 con estilo de moneda
-  const formatted = new Intl.NumberFormat(locale, {
+  return new Intl.NumberFormat(locale, {
     style: "currency",
-    currency: currencyCode,
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(0);
-
-  // Extrae todo lo que no sean dígitos, puntos o comas
-  // Esto deja el símbolo (por ejemplo, "€ 0" → "€ ")
-  const symbol = formatted.replace(/[\d\s.,]/g, "").trim();
-  return symbol;
+    currency,
+    currencyDisplay: "symbol",
+  }).format(amount);
 }
