e5f0a4af030ca3daadd6dc3b4d293b2a4cccc1a5
mDev <miguelcodeonline@gmail.com>
2025-05-02 22:38:25 +0200

    Add TODO

--- client/src/components/ui/SharedCard/SharedCard.tsx
+++ client/src/components/ui/SharedCard/SharedCard.tsx
@@ -3,6 +3,11 @@ import { ReactNode } from "react";
 type SharedCard = {
   children: ReactNode;
 };
+
+/*
+    TODO: Delete SharedCard component when updated card components tested (customer, project, employee cards)
+*/
+
 export const SharedCard = ({ children }: SharedCard) => {
   return (
     <div className="bg-ds-white rounded shadow-ds-2 w-fit h-fit">
