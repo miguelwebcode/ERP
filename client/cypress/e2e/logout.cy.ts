ca27139e5f6d831eb73ecee994f6a6f3c598571c
mDev <miguelcodeonline@gmail.com>
2025-06-14 19:38:09 +0200

    Solve logout test

--- client/cypress/e2e/logout.cy.ts
+++ client/cypress/e2e/logout.cy.ts
@@ -2,13 +2,16 @@ describe("Logout", () => {
   beforeEach(() => {
     cy.logout();
 
-    cy.login();
     cy.visit("/");
+    cy.get("input[name='email']").type("email@email.com");
+
+    cy.get("input[name='password']").type("123456");
+    cy.get("button[type='submit']").click();
   });
 
   it("should navigate from home to login route after logout", () => {
     cy.url().should("eq", "http://localhost:5173/");
-    cy.get("button").filter(":contains('Logout')").click();
+    cy.get('a[href="/login"]').click();
     cy.url().should("eq", "http://localhost:5173/login");
     cy.contains("h1", "Login");
   });
