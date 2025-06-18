971c05ea6b61d5bc284317247a1d49416b9d7e6f
mDev <miguelcodeonline@gmail.com>
2025-06-18 19:06:00 +0200

    Add functions to emulator

--- client/src/firebaseConfig.ts
+++ client/src/firebaseConfig.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
-import { getFunctions } from "firebase/functions";
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,6 +26,7 @@ const functions = getFunctions(app, "europe-west1");
 if (import.meta.env.VITE_MODE === "test") {
   connectFirestoreEmulator(db, "127.0.0.1", 8080);
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFunctionsEmulator(functions, "127.0.0.1", 5001);
 }
 
 export { app, db, auth, functions };
