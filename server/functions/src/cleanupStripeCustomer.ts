dd843bdb227f4224af4525469435f4ac5fbc5b19
mDev <miguelcodeonline@gmail.com>
2025-09-07 20:29:16 +0200

    Add trycatch block

--- server/functions/src/cleanupStripeCustomer.ts
+++ server/functions/src/cleanupStripeCustomer.ts
@@ -5,25 +5,29 @@ export const cleanupStripeCustomer = functions
   .region("europe-west1")
   .auth.user()
   .onDelete(async (user) => {
-    // 1. Obtener Stripe Customer ID
-    const custSnap = await db.doc(`stripeCustomers/${user.uid}`).get();
-    const stripeCustomerId = custSnap.data()?.stripeCustomerId;
-    if (stripeCustomerId) {
-      // 2. Borrar cliente en Stripe
-      await stripe.customers.del(stripeCustomerId);
+    try {
+      // 1. Obtener Stripe Customer ID
+      const custSnap = await db.doc(`stripeCustomers/${user.uid}`).get();
+      const stripeCustomerId = custSnap.data()?.stripeCustomerId;
+      if (stripeCustomerId) {
+        // 2. Borrar cliente en Stripe
+        await stripe.customers.del(stripeCustomerId);
+      }
+
+      // 3. Eliminar suscripciones
+      const subsSnap = await db
+        .collection("subscriptions")
+        .where("stripeCustomerId", "==", stripeCustomerId)
+        .get();
+      subsSnap.forEach((doc) => doc.ref.delete()); //
+
+      // 4. Eliminar pagos únicos
+      const paysSnap = await db
+        .collection("payments")
+        .where("stripeCustomerId", "==", stripeCustomerId)
+        .get();
+      paysSnap.forEach((doc) => doc.ref.delete());
+    } catch (error: any) {
+      console.error('Error cleaning up Stripe customer data:', error);
     }
-
-    // 3. Eliminar suscripciones
-    const subsSnap = await db
-      .collection("subscriptions")
-      .where("stripeCustomerId", "==", stripeCustomerId)
-      .get();
-    subsSnap.forEach((doc) => doc.ref.delete()); //
-
-    // 4. Eliminar pagos únicos
-    const paysSnap = await db
-      .collection("payments")
-      .where("stripeCustomerId", "==", stripeCustomerId)
-      .get();
-    paysSnap.forEach((doc) => doc.ref.delete());
   });
